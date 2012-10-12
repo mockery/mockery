3af51356bb808ae0d4292e7a1b54e1687798f95b
Igor Wiedler <igor@wiedler.ch>
2012-10-13 00:24:08 +0200

    Make generated mock code more readable

--- library/Mockery/Generator.php
+++ library/Mockery/Generator.php
@@ -67,13 +67,14 @@ class Generator
                 $result = self::applyMockeryTo($data['class'], $data['publicMethods'], $block, $partialMethods);
                 if ($result['callTypehinting']) $callTypehinting = true;
                 $definition .= $result['definition'];
+                $definition .= PHP_EOL;
                 $definition .= self::stubAbstractProtected($data['protectedMethods']);
             }  else {
                 $useStandardMethods = false;
             }
         }
         if ($useStandardMethods) $definition .= self::_getStandardMethods($callTypehinting, $makeInstanceMock);
-        $definition .= PHP_EOL . '}';
+        $definition .= PHP_EOL . '}'.PHP_EOL;
 
         return $definition;
     }
@@ -231,7 +232,7 @@ BODY;
         }
         $returnByRef = $method->returnsReference() ? ' & ' : '';
         return $access . ' function ' . $returnByRef . $name . '(' . $paramDef . ')'
-                      . '{' . $body . '}';
+                      . '{' . PHP_EOL . $body . PHP_EOL . '}' . PHP_EOL;
     }
 
     protected static function _renderPublicMethodParameters(\ReflectionMethod $method)
