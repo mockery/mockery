c5ef4c25e9954371065ba49bf023f7711712704a
Dave Marshall <dave.marshall@atstsolutions.co.uk>
2013-05-13 22:35:40 +0100

    Add extension to make debugging easier

--- library/Mockery/Loader/RequireLoader.php
+++ library/Mockery/Loader/RequireLoader.php
@@ -13,7 +13,7 @@ class RequireLoader implements Loader
             return;
         }
 
-        $tmpfname = tempnam(sys_get_temp_dir(), "Mockery");
+        $tmpfname = tempnam(sys_get_temp_dir(), "Mockery") . ".php";
         file_put_contents($tmpfname, $definition->getCode());
 
         require $tmpfname;
