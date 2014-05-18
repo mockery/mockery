4fe03710f35ff4d81c6166e5ebae52adc4b8b178
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2014-05-18 23:27:39 +0200

    add inital test for issue #332: two chain mocks on the same mock object cause error

---
+++ tests/Mockery/DemeterChainTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Mockery
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://github.com/padraic/mockery/master/LICENSE
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to padraic@php.net so we can send you a copy immediately.
+ *
+ * @category   Mockery
+ * @package    Mockery
+ * @subpackage UnitTests
+ * @copyright  Copyright (c) 2010-2014 Pádraic Brady (http://blog.astrumfutura.com)
+ * @license    http://github.com/padraic/mockery/blob/master/LICENSE New BSD License
+ */
+
+
+class DemeterChainTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDemeterChain()
+    {
+        $mock = Mockery::mock('object')->shouldIgnoreMissing();
+
+        $mock->shouldReceive('getElement->getType')
+            ->once()
+            ->andReturn('somethingElse');
+
+        $this->assertEquals('somethingElse', $mock->getElement()->getType());
+
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testTwoChains()
+    {
+        $mock = Mockery::mock('object')->shouldIgnoreMissing();
+        $mock->shouldReceive('getElement->getConfig')
+            ->andReturn('something');
+
+        $mock->shouldReceive('getElement->getType')
+            ->once()
+            ->andReturn('somethingElse');
+
+        $mock->getElement()->getType();
+    }
+}
