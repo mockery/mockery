7531c524f76bf7e0d11246ed610c41d7a14ccfc6
Dave Marshall <dave@atst.io>
2014-10-06 12:57:37 +0100

    Add MockeryTestCase for better PHPUnit integration

---
+++ library/Mockery/Adapter/Phpunit/MockeryTestCase.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Mockery\Adapter\Phpunit;
+
+use Mockery;
+
+class MockeryTestCase extends \PHPUnit_Framework_TestCase
+{
+    protected function assertPostConditions()
+    {
+        $this->addMockeryExpectationsToAssertionCount();
+        $this->closeMockery();
+
+        parent::assertPostConditions();
+    } 
+
+    protected function addMockeryExpectationsToAssertionCount()
+    {
+        $container = Mockery::getContainer();
+        if ($container != null) {
+            $count = $container->mockery_getExpectationCount();
+            $this->addToAssertionCount($count);
+        }
+    }
+
+    protected function closeMockery()
+    {
+        Mockery::close();
+    }
+}
