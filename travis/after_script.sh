8dc06f8ba45ab4937fe275d1f3cab804192280d1
Padraic Brady <padraic.brady@gmail.com>
2015-02-15 14:34:37 +0000

    cleanup after_script.sh

--- travis/after_script.sh
+++ travis/after_script.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-if [[ "$TRAVIS_PHP_VERSION" != "hhvm" && "$TRAVIS_PHP_VERSION" != "hhvm-nightly" ]]; then
+if [[ "$TRAVIS_PHP_VERSION" == "5.6" ]]; then
   vendor/bin/coveralls -v
-fi
-
-if [[ "$TRAVIS_PHP_VERSION" == "5.6" ]]; then
   wget https://scrutinizer-ci.com/ocular.phar
-fi
-
-if [[ "$TRAVIS_PHP_VERSION" == "5.6" ]]; then
   php ocular.phar code-coverage:upload --format=php-clover ./build/logs/clover.xml
 fi
