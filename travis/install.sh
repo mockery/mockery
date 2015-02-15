554c1082fabec91c50721a5b145a0c605512bd86
Padraic Brady <padraic.brady@gmail.com>
2015-02-15 14:35:59 +0000

    clanup install.sh; scoping of what needs coveralls

--- travis/install.sh
+++ travis/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 if [[ "$TRAVIS_PHP_VERSION" == "5.3.3" ]]; then
-    export PATH=$PWD/phar:$PATH
-    echo PATH is $PATH
+  export PATH=$PWD/phar:$PATH
+  echo PATH is $PATH
 fi
+
 composer install -n
-composer require --dev satooshi/php-coveralls:~0.7@dev
+
+if [[ "$TRAVIS_PHP_VERSION" == "5.6" ]]; then
+  composer require --dev satooshi/php-coveralls:~0.7@dev
+fi
