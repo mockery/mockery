526112f9f78bae08c51bf7d9fc988b7b270d7836
Tomas Votruba <tomas.vot@gmail.com>
2015-02-26 21:57:02 +0100

    travis: use native PHP 7.0

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-if [[ "$TRAVIS_PHP_VERSION" != "hhvm" \
-  && "$TRAVIS_PHP_VERSION" != "hhvm-nightly" \
-  && "$TRAVIS_PHP_VERSION" != "5.3.3" ]]; then
+if [[ $TRAVIS_PHP_VERSION != "hhvm" \
+  && $TRAVIS_PHP_VERSION != "hhvm-nightly" \
+  && $TRAVIS_PHP_VERSION != "7.0" ]]; then
   phpenv config-add ./travis/extra.ini
   phpenv rehash
 fi
 
-if [[ "$TRAVIS_PHP_VERSION" == "5.3.3" ]]; then
-  php --version
-fi
-
-if [[ "$TRAVIS_PHP_VERSION" == "5.6" ]]; then
+if [[ $TRAVIS_PHP_VERSION == "5.6" ]]; then
   sed '/MockeryPHPUnitIntegration/d' -i ./phpunit.xml.dist
 fi
