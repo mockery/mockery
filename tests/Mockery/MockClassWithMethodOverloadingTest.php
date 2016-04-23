a08916a767654ec96d0911fc37755764325ab3b9
Nobu Funaki <nobu.funaki@gmail.com>
2016-04-23 12:49:52 -0700

    Add a test for method overloading Make sure the class being mocked has no __call

---
+++ tests/Mockery/MockClassWithMethodOverloadingTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace test\Mockery;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+class MockClassWithMethodOverloadingTest extends MockeryTestCase
+{
+    private $container;
+
+    protected function setUp()
+    {
+        $this->container = new \Mockery\Container;
+    }
+
+    protected function tearDown()
+    {
+        $this->container->mockery_close();
+    }
+
+    /**
+     * @expectedException BadMethodCallException
+     */
+    public function testCreateMockForClassWithMethodOverloading()
+    {
+        $mock = $this->container->mock('test\Mockery\TestWithMethodOverloading')
+            ->makePartial();
+        $this->assertInstanceOf('test\Mockery\TestWithMethodOverloading', $mock);
+
+        // TestWithMethodOverloading::__call wouldn't be used. See Gotchas!.
+        $mock->randomMethod();
+    }
+}
+
+class TestWithMethodOverloading
+{
+    public function __call($name, $arguments)
+    {
+        return 1;
+    }
+}
