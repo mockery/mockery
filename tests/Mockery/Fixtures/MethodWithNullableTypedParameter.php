742f1e852518f905a1481183842b4165566573c8
Dave Marshall <dave@atst.io>
2016-10-12 14:48:53 +0100

    Adds a few more methods to check coverage for nullable parameters

--- tests/Mockery/Fixtures/MethodWithNullableTypedParameter.php
+++ tests/Mockery/Fixtures/MethodWithNullableTypedParameter.php
@@ -24,4 +24,8 @@ namespace test\Mockery\Fixtures;
 class MethodWithNullableTypedParameter
 {
     public function foo(?string $bar) {}
+
+    public function bar(string $bar = null) {}
+
+    public function baz(?string $bar = null) {}
 }
