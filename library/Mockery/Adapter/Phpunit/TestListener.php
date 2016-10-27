536ed44867bd998b0941a78ca881d3d9176207cc
Peter Schultz <peter.schultz@classmarkets.com>
2016-10-27 10:58:15 +0200

    Restore the PHPUnit listener
    The listener has been removed in ede143167ae34e because it is called too
    late in the test lifecycle. This marks tests incorrectly as risky, if
    there are no other assertions in the test.
    
    Simply removing the listener silently breaks existing testsuites for
    PHPUnit <5.5.6, which will not complain about missing listeners.
    
    The listener added in this commit will no longer verify mock assertions.
    Instead, the listener will mark tests as failed if they don't verify the
    mock assertions themselves.
    
    This will surface the change for existing users of the listener and help them
    migrate to the trait or base class. Is also serves as an opt-in safefail for
    new Mockery users.
    
    Fixes #636

---
+++ library/Mockery/Adapter/Phpunit/TestListener.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Mockery
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://github.com/padraic/mockery/blob/master/LICENSE
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to padraic@php.net so we can send you a copy immediately.
+ *
+ * @category   Mockery
+ * @package    Mockery
+ * @copyright  Copyright (c) 2010 Pádraic Brady (http://blog.astrumfutura.com)
+ * @license    http://github.com/padraic/mockery/blob/master/LICENSE New BSD License
+ */
+
+namespace Mockery\Adapter\Phpunit;
+
+class TestListener extends \PHPUnit_Framework_BaseTestListener
+{
+    /**
+     * endTest is called after each test and checks if \Mockery::close() has
+     * been called, and will let the test fail if it hasn't.
+     *
+     * @param  PHPUnit_Framework_Test $test
+     * @param  float                  $time
+     */
+    public function endTest(\PHPUnit_Framework_Test $test, $time)
+    {
+        try {
+            // The self() call is used as a sentinel. Anything that throws if
+            // the container is closed already will do.
+            \Mockery::self();
+        } catch (\LogicException $_) {
+            return;
+        }
+
+        $e = new \PHPUnit_Framework_ExpectationFailedException(sprintf(
+            "Mockery's expectations have not been verified. Make sure that \Mockery::close() is called at the end of the test. Consider using %s\MockeryPHPUnitIntegration or extending %s\MockeryTestCase.",
+            __NAMESPACE__,
+            __NAMESPACE__
+        ));
+        $result = $test->getTestResultObject();
+        $result->addFailure($test, $e, $time);
+    }
+}
