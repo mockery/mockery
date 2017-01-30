3c90be80d71f3900b84546103554e719f01f4442
Dave Marshall <dave@atst.io>
2017-01-30 11:52:56 +0000

    Removed redundant test bootstrapping

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -30,13 +30,7 @@ function isAbsolutePath($path)
     return ($path[0] === DIRECTORY_SEPARATOR) || (preg_match($windowsPattern, $path) === 1);
 }
 
-/*
- * Determine the root, library, and tests directories of the framework
- * distribution.
- */
 $root    = realpath(dirname(dirname(__FILE__)));
-$library = $root . DIRECTORY_SEPARATOR . 'library';
-$tests   = $root . DIRECTORY_SEPARATOR . 'tests';
 $composerVendorDirectory = getenv("COMPOSER_VENDOR_DIR") ?: "vendor";
 
 if (!isAbsolutePath($composerVendorDirectory)) {
@@ -54,17 +48,7 @@ if (!file_exists($autoloadPath)) {
     );
 }
 
-/*
- * Prepend the Mutateme library/ and tests/ directories to the
- * include_path. This allows the tests to run out of the box and helps prevent
- * loading other copies of the code and tests that would supercede
- * this copy.
- */
-$path = array(
-    $library, // required for `testCallingRegisterRegistersSelfAsSplAutoloaderFunction`
-    get_include_path(),
-);
-set_include_path(implode(PATH_SEPARATOR, $path));
+require_once $autoloadPath;
 
 $hamcrestRelativePath = 'hamcrest/hamcrest-php/hamcrest/Hamcrest.php';
 if (DIRECTORY_SEPARATOR !== '/') {
@@ -74,29 +58,7 @@ $hamcrestPath = $composerVendorDirectory . DIRECTORY_SEPARATOR . $hamcrestRelati
 
 require_once $hamcrestPath;
 
-if (defined('TESTS_GENERATE_REPORT') && TESTS_GENERATE_REPORT === true &&
-    version_compare(PHPUnit_Runner_Version::id(), '3.1.6', '>=')) {
-
-    /*
-     * Add Mutateme library/ directory to the PHPUnit code coverage
-     * whitelist. This has the effect that only production code source files
-     * appear in the code coverage report and that all production code source
-     * files, even those that are not covered by a test yet, are processed.
-     */
-    PHPUnit_Util_Filter::addDirectoryToWhitelist($library);
-
-    /*
-     * Omit from code coverage reports the contents of the tests directory
-     */
-    foreach (array('.php', '.phtml', '.csv', '.inc') as $suffix) {
-        PHPUnit_Util_Filter::addDirectoryToFilter($tests, $suffix);
-    }
-    PHPUnit_Util_Filter::addDirectoryToFilter(PHP_LIBDIR);
-}
-
-require $autoloadPath;
-
 /*
  * Unset global variables that are no longer needed.
  */
-unset($root, $library, $tests, $path);
+unset($root, $autoloadPath, $hamcrestPath, $composerVendorDirectory);
