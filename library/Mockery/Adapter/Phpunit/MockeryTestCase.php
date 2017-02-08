91f6577aee7a280e11e74bd6468b28ca82953549
Dave Marshall <dave@atst.io>
2017-02-08 10:36:19 +0000

    Removes leading slash

--- library/Mockery/Adapter/Phpunit/MockeryTestCase.php
+++ library/Mockery/Adapter/Phpunit/MockeryTestCase.php
@@ -22,7 +22,7 @@ namespace Mockery\Adapter\Phpunit;
 
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
-if (class_exists("\PHPUnit_Framework_TestCase")) {
+if (class_exists("PHPUnit_Framework_TestCase")) {
     abstract class MockeryTestCase extends \PHPUnit_Framework_TestCase
     {
         use MockeryPHPUnitIntegration;
