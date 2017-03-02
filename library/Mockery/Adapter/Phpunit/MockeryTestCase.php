35de8e812fe404c1d1ad37c5d6c2ab512f7a8458
Thomas Flori <tflori@pb-systems.de>
2017-03-02 16:37:07 +0100

    readd the trait for backward compatibility

--- library/Mockery/Adapter/Phpunit/MockeryTestCase.php
+++ library/Mockery/Adapter/Phpunit/MockeryTestCase.php
@@ -20,33 +20,7 @@
 
 namespace Mockery\Adapter\Phpunit;
 
-use Mockery;
-
 abstract class MockeryTestCase extends \PHPUnit\Framework\TestCase
 {
-    /**
-     * Performs assertions shared by all tests of a test case. This method is
-     * called before execution of a test ends and before the tearDown method.
-     */
-    protected function assertPostConditions()
-    {
-        $this->addMockeryExpectationsToAssertionCount();
-        $this->closeMockery();
-
-        parent::assertPostConditions();
-    }
-
-    protected function addMockeryExpectationsToAssertionCount()
-    {
-        $container = Mockery::getContainer();
-        if ($container != null) {
-            $count = $container->mockery_getExpectationCount();
-            $this->addToAssertionCount($count);
-        }
-    }
-
-    protected function closeMockery()
-    {
-        Mockery::close();
-    }
+    use MockeryPHPUnitIntegration;
 }
