2fb26a5ae1df5425587a66ea40a90ce7e1884c51
Dave Marshall <dave@atst.io>
2017-08-21 10:36:20 +0100

    Removes redundant class name

--- tests/Mockery/DemeterChainTest.php
+++ tests/Mockery/DemeterChainTest.php
@@ -28,7 +28,7 @@ class DemeterChainTest extends MockeryTestCase
 
     public function setUp()
     {
-        $this->mock = $this->mock = Mockery::mock('object')->shouldIgnoreMissing();
+        $this->mock = $this->mock = Mockery::mock()->shouldIgnoreMissing();
     }
 
     public function tearDown()
