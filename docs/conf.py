128608f262b44360f7e3e86e6eae73f448398711
Dave Marshall <dave@atst.io>
2017-10-11 12:37:29 +0100

    Corrects config for readthedocs latex build

--- docs/conf.py
+++ docs/conf.py
@@ -187,7 +187,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index2', 'MockeryDocs.tex', u'Mockery Docs Documentation',
+  ('index', 'MockeryDocs.tex', u'Mockery Docs Documentation',
    u'Pádraic Brady, Dave Marshall, Wouter, Graham Campbell', 'manual'),
 ]
 
@@ -217,7 +217,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index2', 'mockerydocs', u'Mockery Docs Documentation',
+    ('index', 'mockerydocs', u'Mockery Docs Documentation',
      [u'Pádraic Brady, Dave Marshall, Wouter, Graham Campbell'], 1)
 ]
 
@@ -231,7 +231,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index2', 'MockeryDocs', u'Mockery Docs Documentation',
+  ('index', 'MockeryDocs', u'Mockery Docs Documentation',
    u'Pádraic Brady, Dave Marshall, Wouter, Graham Campbell', 'MockeryDocs', 'One line description of project.',
    'Miscellaneous'),
 ]
