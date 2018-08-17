bac41d3265b1d6a6f555452607cee899306eb275
Alex Denvir <alexdenvir@protecinnovations.co.uk>
2018-08-17 15:53:22 +0100

    Add PhpStorm Advanced Metadata for autocompletion

---
+++ .phpstorm.meta.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PHPSTORM_META;
+
+override(\Mockery::mock(0), type(0));
+override(\Mockery::spy(0), type(0));
+override(\Mockery::namedMock(0), type(0));
+override(\Mockery::instanceMock(0), type(0));
+override(\mock(0), type(0));
+override(\spy(0), type(0));
+override(\namedMock(0), type(0));
