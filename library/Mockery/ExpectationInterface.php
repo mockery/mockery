184c5eafa5da382c5a3ddfb4ba614f9e33c79ae2
slepic <gslepic@gmail.com>
2019-09-05 12:06:08 +0200

    added methods of Expectation class to ExpectationInterface

--- library/Mockery/ExpectationInterface.php
+++ library/Mockery/ExpectationInterface.php
@@ -34,6 +34,37 @@ interface ExpectationInterface
     public function getMock();
 
     /**
+     * Expected argument setter for the expectation
+     *
+     * @param mixed[] ...$args
+     * @return self
+     */
+    public function with(...$args);
+
+    /**
+     * Expected arguments for the expectation passed as an array or a closure that matches each passed argument on
+     * each function call.
+     *
+     * @param array|\Closure $argsOrClosure
+     * @return self
+     */
+    public function withArgs($argsOrClosure);
+
+    /**
+     * Set with() as no arguments expected
+     *
+     * @return self
+     */
+    public function withNoArgs();
+
+    /**
+     * Set expectation that any arguments are acceptable
+     *
+     * @return self
+     */
+    public function withAnyArgs();
+
+    /**
      * @param array ...$args
      * @return self
      */
@@ -43,4 +74,63 @@ interface ExpectationInterface
      * @return self
      */
     public function andReturns();
+
+    /**
+     * Indicates the number of times this expectation should occur
+     *
+     * @param int $limit
+     * @throws \InvalidArgumentException
+     * @return self
+     */
+    public function times($limit = null);
+
+    /**
+     * Indicates that this expectation is never expected to be called
+     *
+     * @return self
+     */
+    public function never();
+
+    /**
+     * Indicates that this expectation is expected exactly once
+     *
+     * @return self
+     */
+    public function once();
+
+    /**
+     * Indicates that this expectation is expected exactly twice
+     *
+     * @return self
+     */
+    public function twice();
+
+    /**
+     * Sets next count validator to the AtLeast instance
+     *
+     * @return self
+     */
+    public function atLeast();
+
+    /**
+     * Sets next count validator to the AtMost instance
+     *
+     * @return self
+     */
+    public function atMost();
+
+    /**
+     * Indicates this expectation should occur zero or more times
+     *
+     * @return self
+     */
+    public function zeroOrMoreTimes();
+
+    /**
+     * Shorthand for setting minimum and maximum constraints on call counts
+     *
+     * @param int $minimum
+     * @param int $maximum
+     */
+    public function between($minimum, $maximum);
 }
