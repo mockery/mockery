70db9b2c8202e85179a20ef95d7fdb917e8e697f
Ivan Fedorov <ivan.fedorov@jetbrains.com>
2021-05-25 19:41:34 +0300

    update meta to properly infer type of string parameters

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -2,10 +2,10 @@
 
 namespace PHPSTORM_META;
 
-override(\Mockery::mock(0), type(0));
-override(\Mockery::spy(0), type(0));
-override(\Mockery::namedMock(0), type(0));
-override(\Mockery::instanceMock(0), type(0));
-override(\mock(0), type(0));
-override(\spy(0), type(0));
-override(\namedMock(0), type(0));
+override(\Mockery::mock(0), map(["" => "$0"]));
+override(\Mockery::spy(0), map(["" => "$0"]));
+override(\Mockery::namedMock(0), map(["" => "$0"]));
+override(\Mockery::instanceMock(0), map(["" => "$0"]));
+override(\mock(0), map(["" => "$0"]));
+override(\spy(0), map(["" => "$0"]));
+override(\namedMock(0), map(["" => "$0"]));
