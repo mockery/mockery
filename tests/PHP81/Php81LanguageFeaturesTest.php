ec2443ad05d816cc5fd04ad6a4b8234ec81e3f0e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-06-08 16:38:58 -0400

    Test never-returning function/method that terminates
    Co-Authored-By: Dave Marshall <dave@atstsolutions.co.uk>
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/PHP81/Php81LanguageFeaturesTest.php
+++ tests/PHP81/Php81LanguageFeaturesTest.php
@@ -8,6 +8,9 @@ use Mockery\Adapter\Phpunit\MockeryTestCase;
 use ReturnTypeWillChange;
 use RuntimeException;
 use Serializable;
+use function pcntl_fork;
+use function pcntl_waitpid;
+use function pcntl_wexitstatus;
 
 /**
  * @requires PHP 8.1.0-dev
@@ -92,6 +95,30 @@ class Php81LanguageFeaturesTest extends MockeryTestCase
         $this->expectException(RuntimeException::class);
         $mock->throws();
     }
+
+    /**
+     * @requires extension pcntl
+     * @test
+     */
+    public function it_can_mock_a_class_with_a_never_returning_type_hint_with_exit()
+    {
+        $mock = Mockery::mock(NeverReturningTypehintClass::class)->makePartial();
+
+        $pid = pcntl_fork();
+
+        if (-1 === $pid) {
+            $this->markTestSkipped("Couldn't fork for exit test");
+
+            return;
+        } elseif ($pid) {
+            pcntl_waitpid($pid, $status);
+            $this->assertEquals(123, pcntl_wexitstatus($status));
+
+            return;
+        }
+
+        $mock->exits();
+    }
 }
 
 interface LoggerInterface
@@ -154,7 +181,7 @@ class NeverReturningTypehintClass
 
     public function exits(): never
     {
-        exit;
+        exit(123);
     }
 }
 class IntersectionTypeHelperClass
