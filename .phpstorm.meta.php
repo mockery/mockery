252c3a9e756f73df8d67af6101993a2f77282e17
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-14 18:31:59 -0500

    Update .phpstorm.meta.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>
    Co-Authored-By: Aleksei Khudiakov <aleksey@xerkus.pro>

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -2,10 +2,10 @@
 
 namespace PHPSTORM_META;
 
-override(\Mockery::mock(0), map(["" => "@"]));
-override(\Mockery::spy(0), map(["" => "@"]));
-override(\Mockery::namedMock(0), map(["" => "@"]));
-override(\Mockery::instanceMock(0), map(["" => "@"]));
-override(\mock(0), map(["" => "@"]));
-override(\spy(0), map(["" => "@"]));
-override(\namedMock(0), map(["" => "@"]));
+override(\Mockery::mock(0), map(["" => "@&\Mockery\MockInterface"]));
+override(\Mockery::spy(0), map(["" => "@&\Mockery\MockInterface"]));
+override(\Mockery::namedMock(0), map(["" => "@&\Mockery\MockInterface"]));
+override(\Mockery::instanceMock(0), map(["" => "@&\Mockery\MockInterface"]));
+override(\mock(0), map(["" => "@&\Mockery\MockInterface"]));
+override(\spy(0), map(["" => "@&\Mockery\MockInterface"]));
+override(\namedMock(0), map(["" => "@&\Mockery\MockInterface"]));
