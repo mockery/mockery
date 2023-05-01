56227d570e86c10315b1c067c8ab0475fbaabc90
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-30 21:58:59 -0500

    Update Expectation.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- library/Mockery/Expectation.php
+++ library/Mockery/Expectation.php
@@ -399,7 +399,7 @@ class Expectation implements ExpectationInterface
             return $expected->match($actual);
         }
         if ($expected instanceof \Hamcrest\Matcher || $expected instanceof \Hamcrest_Matcher) {
-            trigger_error('Hamcrest package has been deprecated and will be removed in 2.0', E_USER_DEPRECATED);
+            @trigger_error('Hamcrest package has been deprecated and will be removed in 2.0', E_USER_DEPRECATED);
 
             return $expected->matches($actual);
         }
