3e2ffe0a15544dd8d7b9519bcff95016f8fec5e9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-05-03 22:09:45 -0500

    Update HasValue.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- library/Mockery/Matcher/HasValue.php
+++ library/Mockery/Matcher/HasValue.php
@@ -1,46 +1,18 @@
 <?php
-/**
- * Mockery
- *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://github.com/padraic/mockery/blob/master/LICENSE
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to padraic@php.net so we can send you a copy immediately.
- *
- * @category   Mockery
- * @package    Mockery
- * @copyright  Copyright (c) 2010 Pádraic Brady (http://blog.astrumfutura.com)
- * @license    http://github.com/padraic/mockery/blob/master/LICENSE New BSD License
- */
+
+declare(strict_types=1);
 
 namespace Mockery\Matcher;
 
-class HasValue extends MatcherAbstract
+final class HasValue extends AbstractMatcher
 {
-    /**
-     * Check if the actual value matches the expected.
-     *
-     * @param mixed $actual
-     * @return bool
-     */
-    public function match(&$actual)
+    public function match(mixed &$actual): bool
     {
-        return in_array($this->_expected, $actual);
+        return in_array($this->expected, $actual);
     }
 
-    /**
-     * Return a string representation of this Matcher
-     *
-     * @return string
-     */
-    public function __toString()
+    public function __toString(): string
     {
-        $return = '<HasValue[' . (string) $this->_expected . ']>';
-        return $return;
+        return sprintf('<HasValue[%s]>', $this->expected);
     }
 }
