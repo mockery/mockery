b65980069f5657745d1c27da425ed1a8c568f6f2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-05-03 21:18:35 -0500

    Update Exact.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- library/Mockery/CountValidator/Exact.php
+++ library/Mockery/CountValidator/Exact.php
@@ -1,54 +1,37 @@
 <?php
-/**
- * Mockery
- *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://github.com/padraic/mockery/blob/master/LICENSE
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to padraic@php.net so we can send you a copy immediately.
- *
- * @category   Mockery
- * @package    Mockery
- * @copyright  Copyright (c) 2010 Pádraic Brady (http://blog.astrumfutura.com)
- * @license    http://github.com/padraic/mockery/blob/master/LICENSE New BSD License
- */
+
+declare(strict_types=1);
 
 namespace Mockery\CountValidator;
 
 use Mockery;
 
-class Exact extends CountValidatorAbstract
+final class Exact extends AbstractCountValidator
 {
     /**
      * Validate the call count against this validator
-     *
-     * @param int $n
-     * @return bool
      */
-    public function validate($n)
+    public function validate(int $n): bool
     {
-        if ($this->_limit !== $n) {
-            $because = $this->_expectation->getExceptionMessage();
+        if ($this->limit !== $n) {
+            $because = $this->expectation->getExceptionMessage();
 
             $exception = new Mockery\Exception\InvalidCountException(
-                'Method ' . (string) $this->_expectation
-                . ' from ' . $this->_expectation->getMock()->mockery_getName()
+                'Method ' . (string) $this->expectation
+                . ' from ' . $this->expectation->getMock()->mockery_getName()
                 . ' should be called' . PHP_EOL
-                . ' exactly ' . $this->_limit . ' times but called ' . $n
+                . ' exactly ' . $this->limit . ' times but called ' . $n
                 . ' times.'
-                . ($because ? ' Because ' . $this->_expectation->getExceptionMessage() : '')
+                . ($because ? ' Because ' . $this->expectation->getExceptionMessage() : '')
             );
-            $exception->setMock($this->_expectation->getMock())
-                ->setMethodName((string) $this->_expectation)
+            $exception->setMock($this->expectation->getMock())
+                ->setMethodName((string) $this->expectation)
                 ->setExpectedCountComparative('=')
-                ->setExpectedCount($this->_limit)
+                ->setExpectedCount($this->limit)
                 ->setActualCount($n);
             throw $exception;
         }
+
+        return true;
     }
 }
