62c5564d56c29c0df0acd68fb82e365e2dc09216
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-05-03 20:38:41 -0500

    Update RequireLoader.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- library/Mockery/Loader/RequireLoader.php
+++ library/Mockery/Loader/RequireLoader.php
@@ -1,46 +1,33 @@
 <?php
-/**
- * Mockery
- *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://github.com/padraic/mockery/blob/master/LICENSE
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to padraic@php.net so we can send you a copy immediately.
- *
- * @category   Mockery
- * @package    Mockery
- * @copyright  Copyright (c) 2010 Pádraic Brady (http://blog.astrumfutura.com)
- * @license    http://github.com/padraic/mockery/blob/master/LICENSE New BSD License
- */
+
+declare(strict_types=1);
 
 namespace Mockery\Loader;
 
 use Mockery\Generator\MockDefinition;
-use Mockery\Loader\Loader;
 
-class RequireLoader implements Loader
+final class RequireLoader implements Loader
 {
-    protected $path;
-
-    public function __construct($path = null)
+    public function __construct(private string $path)
     {
-        $this->path = realpath($path) ?: sys_get_temp_dir();
+        $this->path = realpath($this->path) ?: sys_get_temp_dir();
     }
 
-    public function load(MockDefinition $definition)
+    public function load(MockDefinition $definition): void
     {
         if (class_exists($definition->getClassName(), false)) {
             return;
         }
 
-        $tmpfname = $this->path . DIRECTORY_SEPARATOR . "Mockery_" . uniqid() . ".php";
-        file_put_contents($tmpfname, $definition->getCode());
+        $temporaryFilePath = sprintf(
+            '%s%s%s.php',
+            $this->path,
+            DIRECTORY_SEPARATOR,
+            uniqid('Mockery_')
+        );
 
-        require $tmpfname;
+        file_put_contents($temporaryFilePath, $definition->getCode());
+
+        require $temporaryFilePath;
     }
 }
