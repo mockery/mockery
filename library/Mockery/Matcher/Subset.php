a3378052d2ca635b05d08d906c36efcec0ba7620
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-05-03 22:10:40 -0500

    Update Subset.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- library/Mockery/Matcher/Subset.php
+++ library/Mockery/Matcher/Subset.php
@@ -1,67 +1,39 @@
 <?php
-/**
- * Mockery
- *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://github.com/padraic/mockery/blob/master/LICENSE
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to padraic@php.net so we can send you a copy immediately.
- *
- * @category   Mockery
- * @package    Mockery
- * @copyright  Copyright (c) 2010 Pádraic Brady (http://blog.astrumfutura.com)
- * @license    http://github.com/padraic/mockery/blob/master/LICENSE New BSD License
- */
+
+declare(strict_types=1);
 
 namespace Mockery\Matcher;
 
-class Subset extends MatcherAbstract
+final class Subset extends AbstractMatcher
 {
-    private $expected;
-    private $strict = true;
-
     /**
      * @param array $expected Expected subset of data
      * @param bool $strict Whether to run a strict or loose comparison
      */
-    public function __construct(array $expected, $strict = true)
-    {
-        $this->expected = $expected;
-        $this->strict = $strict;
+    public function __construct(
+        array $expected,
+        private readonly bool $strict = true
+    ) {
+        parent::__construct($expected);
     }
 
     /**
      * @param array $expected Expected subset of data
-     *
-     * @return Subset
      */
-    public static function strict(array $expected)
+    public static function strict(array $expected): self
     {
-        return new static($expected, true);
+        return new self($expected, true);
     }
 
     /**
      * @param array $expected Expected subset of data
-     *
-     * @return Subset
      */
     public static function loose(array $expected)
     {
-        return new static($expected, false);
+        return new self($expected, false);
     }
 
-    /**
-     * Check if the actual value matches the expected.
-     *
-     * @param mixed $actual
-     * @return bool
-     */
-    public function match(&$actual)
+    public function match(mixed &$actual): bool
     {
         if (!is_array($actual)) {
             return false;
@@ -74,14 +46,9 @@ class Subset extends MatcherAbstract
         return $actual == array_replace_recursive($actual, $this->expected);
     }
 
-    /**
-     * Return a string representation of this Matcher
-     *
-     * @return string
-     */
-    public function __toString()
+    public function __toString(): string
     {
-        return '<Subset' . $this->formatArray($this->expected) . ">";
+        return sprintf('<Subset%s>', $this->formatArray($this->expected));
     }
 
     /**
@@ -90,12 +57,14 @@ class Subset extends MatcherAbstract
      * @param array $array
      * @return string
      */
-    protected function formatArray(array $array)
+    private function formatArray(array $array): string
     {
         $elements = [];
-        foreach ($array as $k => $v) {
-            $elements[] = $k . '=' . (is_array($v) ? $this->formatArray($v) : (string) $v);
+
+        foreach ($array as $key => $value) {
+            $elements[] = $key . '=' . (is_array($value) ? $this->formatArray($value) : (string) $value);
         }
-        return "[" . implode(", ", $elements) . "]";
+
+        return sprintf('[%s]', implode(', ', $elements));
     }
 }
