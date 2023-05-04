4dc3d4146d7fcd99a578ce08b9577ca166712146
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-05-03 19:36:01 -0500

    Update CachingGenerator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- library/Mockery/Generator/CachingGenerator.php
+++ library/Mockery/Generator/CachingGenerator.php
@@ -1,45 +1,19 @@
 <?php
-/**
- * Mockery
- *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://github.com/padraic/mockery/blob/master/LICENSE
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to padraic@php.net so we can send you a copy immediately.
- *
- * @category   Mockery
- * @package    Mockery
- * @copyright  Copyright (c) 2010 Pádraic Brady (http://blog.astrumfutura.com)
- * @license    http://github.com/padraic/mockery/blob/master/LICENSE New BSD License
- */
+
+declare(strict_types=1);
 
 namespace Mockery\Generator;
 
-class CachingGenerator implements Generator
+final class CachingGenerator implements Generator
 {
-    protected $generator;
-    protected $cache = array();
-
-    public function __construct(Generator $generator)
-    {
-        $this->generator = $generator;
+    private array $cache = [];
+    public function __construct(
+        private readonly Generator $generator
+    ) {
     }
 
-    public function generate(MockConfiguration $config)
+    public function generate(MockConfiguration $config): MockDefinition
     {
-        $hash = $config->getHash();
-        if (isset($this->cache[$hash])) {
-            return $this->cache[$hash];
-        }
-
-        $definition = $this->generator->generate($config);
-        $this->cache[$hash] = $definition;
-
-        return $definition;
+        return $this->cache[$config->getHash()] ??= $this->generator->generate($config);
     }
 }
