1a278e4442afc0beb0df6ac243692e5af54a8a4a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-05-06 12:10:21 -0500

    Update ClassVisitor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- library/Mockery/Parser/NodeVisitor/ClassVisitor.php
+++ library/Mockery/Parser/NodeVisitor/ClassVisitor.php
@@ -4,6 +4,32 @@ declare(strict_types=1);
 
 namespace Mockery\Parser\NodeVisitor;
 
+use Mockery\Generator\DefinedTargetClass;
+use Mockery\Generator\UndefinedTargetClass;
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Class_;
+
 final class ClassVisitor extends AbstractNodeVisitor
 {
+    public function leaveNode(Node $node)
+    {
+        if (!$node instanceof Class_){
+            return null;
+        }
+
+        /** @var DefinedTargetClass|UndefinedTargetClass|null $target */
+        $target = $this->configuration->getTargetClass();
+        if ($target === null || $target->isFinal()) {
+            return null;
+        }
+
+        $className = ltrim($target->getName(), "\\");
+        if (!class_exists($className)) {
+            \Mockery::declareClass($className);
+        }
+
+        $node->extends = new Node\Name($className);
+
+        return $node;
+    }
 }
