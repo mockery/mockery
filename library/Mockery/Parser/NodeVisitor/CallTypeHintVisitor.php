abaa8ba12934ac4b375e992cf5b75bce4c0628e2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-05-06 12:00:21 -0500

    Update CallTypeHintVisitor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- library/Mockery/Parser/NodeVisitor/CallTypeHintVisitor.php
+++ library/Mockery/Parser/NodeVisitor/CallTypeHintVisitor.php
@@ -4,6 +4,35 @@ declare(strict_types=1);
 
 namespace Mockery\Parser\NodeVisitor;
 
+use PhpParser\Node;
+use PhpParser\Node\Stmt\ClassMethod;
+
 final class CallTypeHintVisitor extends AbstractNodeVisitor
 {
+
+    private bool $requiresCallTypeHintRemoval = false;
+    private bool $requiresCallStaticTypeHintRemoval = false;
+
+    public function beforeTraverse(array $nodes)
+    {
+        $this->requiresCallTypeHintRemoval = (bool) $this->configuration->requiresCallTypeHintRemoval();
+        $this->requiresCallStaticTypeHintRemoval = (bool) $this->configuration->requiresCallStaticTypeHintRemoval();
+    }
+
+    public function leaveNode(Node $node)
+    {
+        if (!$node instanceof ClassMethod) {
+            return null;
+        }
+
+        if (!$this->requiresCallTypeHintRemoval && !$this->requiresCallStaticTypeHintRemoval) {
+            return null;
+        }
+
+        foreach ($node->params as &$param){
+            $param->type = null;
+        }
+
+        return $node;
+    }
 }
