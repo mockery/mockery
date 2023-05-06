8233d09a59e0a1baf29c2b69d4df8a9f905b5055
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-05-06 12:12:47 -0500

    Update InterfaceVisitor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- library/Mockery/Parser/NodeVisitor/InterfaceVisitor.php
+++ library/Mockery/Parser/NodeVisitor/InterfaceVisitor.php
@@ -4,6 +4,30 @@ declare(strict_types=1);
 
 namespace Mockery\Parser\NodeVisitor;
 
+use Mockery;
+use Mockery\Generator\DefinedTargetClass;
+use Mockery\Generator\MockConfiguration;
+use Mockery\Generator\UndefinedTargetClass;
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Class_;
+
 final class InterfaceVisitor extends AbstractNodeVisitor
 {
+    public function enterNode(Node $node)
+    {
+        if (!$node instanceof Class_) {
+            return null;
+        }
+
+        /** @var UndefinedTargetClass|DefinedTargetClass $targetInterface */
+        foreach ($this->configuration->getTargetInterfaces() as $targetInterface) {
+            $name = ltrim($targetInterface->getName(), '\\');
+            if (! interface_exists($name)) {
+                Mockery::declareInterface($name);
+            }
+            $node->implements[] = new Node\Name('\\'.$name);
+        }
+
+        return $node;
+    }
 }
