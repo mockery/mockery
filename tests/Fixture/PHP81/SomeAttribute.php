638b2b9b8649ce3ccc12256c18ddc3ee57af08be
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-07-17 18:43:16 -0500

    Fix cs issue
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/PHP81/SomeAttribute.php
+++ tests/Fixture/PHP81/SomeAttribute.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace Fixture\PHP81;
 
 #[\Attribute(\Attribute::TARGET_PARAMETER)]
-class SomeAttribute {
+class SomeAttribute
+{
     public function __construct(
         public object $param = new stdClass()
     ) {
