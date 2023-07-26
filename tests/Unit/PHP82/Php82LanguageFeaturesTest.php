801e3edd650f74e8d4ec47eeac34a66bb4359718
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-07-26 09:17:24 -0500

    Create Php82LanguageFeaturesTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/PHP82/Php82LanguageFeaturesTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Mockery\Tests\Unit\PHP82;
+
+use Fixture\PHP82\Sut;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+/**
+ * @requires PHP 8.2.0-dev
+ */
+class Php82LanguageFeaturesTest extends MockeryTestCase
+{
+    public function testCanMockDisjunctiveNormalFormType(): void
+    {
+        $mock = mock(Sut::class);
+
+        $reflectionClass = new \ReflectionClass($mock);
+        $type = $reflectionClass->getMethod('foo')
+            ->getParameters()[0]
+            ->getType();
+
+        $expectedReflectionClass = new \ReflectionClass(Sut::class);
+        $expectedType = $expectedReflectionClass->getMethod('foo')
+            ->getParameters()[0]
+            ->getType();
+
+        self::assertSame($expectedType->__toString(),$type->__toString());
+    }
+}
