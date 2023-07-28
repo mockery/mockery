33b01a1c12293c3494c99976c9c8d60dd20d7698
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-07-28 11:18:46 -0500

    Update Php82LanguageFeaturesTest
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/PHP82/Php82LanguageFeaturesTest.php
+++ tests/Unit/PHP82/Php82LanguageFeaturesTest.php
@@ -2,28 +2,88 @@
 
 namespace Mockery\Tests\Unit\PHP82;
 
+use Fixture\PHP82\DisjunctiveNormalFormTypes\ParameterContraVariance;
+use Fixture\PHP82\DisjunctiveNormalFormTypes\ReturnCoVariance;
 use Fixture\PHP82\Sut;
+use Generator;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Reflection;
+use ReflectionType;
 
 /**
  * @requires PHP 8.2.0-dev
  */
 class Php82LanguageFeaturesTest extends MockeryTestCase
 {
-    public function testCanMockDisjunctiveNormalFormTypes(): void
+    /**
+     * @param class-string $fullyQualifiedClassName
+     * @dataProvider parameterContraVarianceDataProvider
+     */
+    public function testMockParameterDisjunctiveNormalFormTypes(string $fullyQualifiedClassName): void
     {
-        $mock = mock(Sut::class);
+        $expectedReflectionClass = new \ReflectionClass($fullyQualifiedClassName);
+        $expectedMethod = $expectedReflectionClass->getMethods()[0];
+        $expectedType = $expectedMethod
+            ->getParameters()[0]
+            ->getType();
+
+        $mock = mock($fullyQualifiedClassName);
 
         $reflectionClass = new \ReflectionClass($mock);
-        $type = $reflectionClass->getMethod('foo')
+        $type = $reflectionClass->getMethod($expectedMethod->getName())
             ->getParameters()[0]
             ->getType();
 
-        $expectedReflectionClass = new \ReflectionClass(Sut::class);
-        $expectedType = $expectedReflectionClass->getMethod('foo')
-            ->getParameters()[0]
-            ->getType();
+        self::assertSame($expectedType->__toString(), $type->__toString());
+    }
 
-        self::assertSame($expectedType->__toString(),$type->__toString());
+    /**
+     * @param class-string $fullyQualifiedClassName
+     * @dataProvider returnCoVarianceDataProvider
+     */
+    public function testMockReturnDisjunctiveNormalFormTypes(string $fullyQualifiedClassName): void
+    {
+        $expectedReflectionClass = new \ReflectionClass($fullyQualifiedClassName);
+        $expectedMethod = $expectedReflectionClass->getMethods()[0];
+        $expectedType = $expectedMethod->getReturnType();
+
+        self::assertInstanceOf(ReflectionType::class, $expectedType);
+
+        $mock = mock($fullyQualifiedClassName);
+
+        $reflectionClass = new \ReflectionClass($mock);
+
+        $type = $reflectionClass->getMethod($expectedMethod->getName())
+            ->getReturnType();
+
+        self::assertInstanceOf(ReflectionType::class, $type);
+
+        self::assertSame($expectedType->__toString(), $type->__toString());
+    }
+
+    public static function parameterContraVarianceDataProvider(): Generator
+    {
+        $fixtures = [
+            Sut::class,
+            ParameterContraVariance\TestOne::class,
+            ParameterContraVariance\TestTwo::class,
+            ParameterContraVariance\TestThree::class,
+        ];
+
+        foreach ($fixtures as $fixture) {
+            yield $fixture => [$fixture];
+        }
+    }
+    public static function returnCoVarianceDataProvider(): Generator
+    {
+        $fixtures = [
+            ReturnCoVariance\TestOne::class,
+            ReturnCoVariance\TestTwo::class,
+            ReturnCoVariance\TestThree::class,
+        ];
+
+        foreach ($fixtures as $fixture) {
+            yield $fixture => [$fixture];
+        }
     }
 }
