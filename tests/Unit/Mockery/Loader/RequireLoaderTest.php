2f2eb83dcc46443059b296e069de375858f0d4dc
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-09-04 09:58:55 -0500

    Create RequireLoaderTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/Loader/RequireLoaderTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Mockery (https://docs.mockery.io/)
+ *
+ * @copyright https://github.com/mockery/mockery/blob/HEAD/COPYRIGHT.md
+ * @license   https://github.com/mockery/mockery/blob/HEAD/LICENSE BSD 3-Clause License
+ * @link      https://github.com/mockery/mockery for the canonical source repository
+ */
+
+namespace Mockery\Tests\Unit\Mockery\Loader;
+
+use Mockery\Loader\RequireLoader;
+
+/**
+ * @covers \Mockery\Loader\RequireLoader
+ * @internal
+ */
+final class RequireLoaderTest extends AbstractLoaderTestCase
+{
+    public function getLoader(): RequireLoader
+    {
+        return new RequireLoader(sys_get_temp_dir());
+    }
+}
