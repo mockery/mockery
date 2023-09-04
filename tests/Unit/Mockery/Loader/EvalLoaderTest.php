52bb92f1dba33fc7a2a6ad07667b4d4d503934f7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-09-04 09:58:53 -0500

    Create EvalLoaderTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/Loader/EvalLoaderTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Mockery (https://docs.mockery.io/)
+ *
+ * @copyright https://github.com/mockery/mockery/blob/HEAD/COPYRIGHT.md
+ * @license   https://github.com/mockery/mockery/blob/HEAD/LICENSE BSD 3-Clause License
+ * @link      https://github.com/mockery/mockery for the canonical source repository
+ */
+
+namespace Mockery\Tests\Unit\Mockery\Loader;
+
+use Mockery\Loader\EvalLoader;
+
+/**
+ * @covers \Mockery\Loader\EvalLoader
+ * @internal
+ */
+final class EvalLoaderTest extends AbstractLoaderTestCase
+{
+    public function getLoader(): EvalLoader
+    {
+        return new EvalLoader();
+    }
+}
