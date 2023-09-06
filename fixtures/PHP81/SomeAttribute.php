6e048cde0d7a03a57d061cfbee4d3a79fef7eedf
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-09-06 15:57:10 -0500

    Add DocBlock

--- fixtures/PHP81/SomeAttribute.php
+++ fixtures/PHP81/SomeAttribute.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/**
+ * Mockery (https://docs.mockery.io/)
+ *
+ * @copyright https://github.com/mockery/mockery/blob/HEAD/COPYRIGHT.md
+ * @license   https://github.com/mockery/mockery/blob/HEAD/LICENSE BSD 3-Clause License
+ * @link      https://github.com/mockery/mockery for the canonical source repository
+ */
+
 namespace PHP81;
 
 use Fixture\PHP81\stdClass;
--- fixtures/autoload.php
+++ fixtures/autoload.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/**
+ * Mockery (https://docs.mockery.io/)
+ *
+ * @copyright https://github.com/mockery/mockery/blob/HEAD/COPYRIGHT.md
+ * @license   https://github.com/mockery/mockery/blob/HEAD/LICENSE BSD 3-Clause License
+ * @link      https://github.com/mockery/mockery for the canonical source repository
+ */
+
 use Composer\Autoload\ClassLoader;
 
 if (!function_exists('recursiveGlob')) {
