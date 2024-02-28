a1e47ac50274b980dda0f29fdb76504db0114bad
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-28 13:34:13 -0600

    Update MethodCall.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- library/Mockery/MethodCall.php
+++ library/Mockery/MethodCall.php
@@ -5,6 +5,7 @@
  *
  * @copyright https://github.com/mockery/mockery/blob/HEAD/COPYRIGHT.md
  * @license   https://github.com/mockery/mockery/blob/HEAD/LICENSE BSD 3-Clause License
+ *
  * @link      https://github.com/mockery/mockery for the canonical source repository
  */
 
@@ -12,22 +13,39 @@ namespace Mockery;
 
 class MethodCall
 {
-    private $method;
+    /**
+     * @var array
+     */
     private $args;
 
+    /**
+     * @var string
+     */
+    private $method;
+
+    /**
+     * @param string $method
+     * @param array  $args
+     */
     public function __construct($method, $args)
     {
         $this->method = $method;
         $this->args = $args;
     }
 
-    public function getMethod()
-    {
-        return $this->method;
-    }
-
+    /**
+     * @return array
+     */
     public function getArgs()
     {
         return $this->args;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
     }
 }
