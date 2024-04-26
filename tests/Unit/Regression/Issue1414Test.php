f2da15f8a2d63f8a802191df213217b90efe3829
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-26 07:29:31 -0500

    Add Issue1414Test.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>
    Co-Authored-By: lew-tfli <45763766+lew-tfli@users.noreply.github.com>

---
+++ tests/Unit/Regression/Issue1414Test.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mockery\Tests\Unit\Regression;
+
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Mockery
+ * @see https://github.com/mockery/mockery/issues/1414
+ */
+final class Issue1414Test extends TestCase
+{
+    public function testMockAnonymousClass(): void
+    {
+        $class = new class() extends \stdClass {};
+
+        $mock = Mockery::mock($class::class);
+
+        self::assertInstanceOf($class::class, $mock);
+    }
+}
