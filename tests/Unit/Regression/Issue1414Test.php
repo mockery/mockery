58e71328905836eb6f416b18dfea04f6f4364a47
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-26 08:42:28 -0500

    '::class' on objects is only allowed since PHP 8.0
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Regression/Issue1414Test.php
+++ tests/Unit/Regression/Issue1414Test.php
@@ -10,6 +10,7 @@ use PHPUnit\Framework\TestCase;
 /**
  * @covers \Mockery
  * @see https://github.com/mockery/mockery/issues/1414
+ * @requires PHP 8.0
  */
 final class Issue1414Test extends TestCase
 {
