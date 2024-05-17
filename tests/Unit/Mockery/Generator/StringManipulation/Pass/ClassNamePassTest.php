0c88e1c99ecc6221820524377e89dd8c90367593
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:25 -0500

    Add ClassNamePassTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/Generator/StringManipulation/Pass/ClassNamePassTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\Mockery\Generator\StringManipulation\Pass;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery\Generator\MockConfiguration;
+use Mockery\Generator\StringManipulation\Pass\ClassNamePass;
+use function mb_strpos;
+
+final class ClassNamePassTest extends MockeryTestCase
+{
+    public const CODE = 'namespace Mockery; class Mock {}';
+
+    protected $pass;
+
+    protected function mockeryTestSetUp(): void
+    {
+        $this->pass = new ClassNamePass();
+    }
+
+    public function testShouldRemoveLeadingBackslashesFromNamespace(): void
+    {
+        $config = new MockConfiguration([], [], [], 'Dave\Dave');
+        $code = $this->pass->apply(static::CODE, $config);
+        self::assertNotFalse(mb_strpos($code, 'namespace Dave;'));
+    }
+
+    public function testShouldRemoveNamespaceDefinition(): void
+    {
+        $config = new MockConfiguration([], [], [], 'Dave\Dave');
+        $code = $this->pass->apply(self::CODE, $config);
+
+        self::assertFalse(mb_strpos($code, 'namespace Mockery;'));
+    }
+
+    public function testShouldReplaceClassNameWithSpecifiedName(): void
+    {
+        $config = new MockConfiguration([], [], [], 'Dave');
+        $code = $this->pass->apply(static::CODE, $config);
+        self::assertNotFalse(mb_strpos($code, 'class Dave'));
+    }
+
+    public function testShouldReplaceNamespaceIfClassNameIsNamespaced(): void
+    {
+        $config = new MockConfiguration([], [], [], 'Dave\Dave');
+        $code = $this->pass->apply(self::CODE, $config);
+        self::assertFalse(mb_strpos($code, 'namespace Mockery;'));
+        self::assertNotFalse(mb_strpos($code, 'namespace Dave;'));
+    }
+}
