a56d048adb7cbbffbc91b34aa5929f03c0e1b9f5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:21 -0500

    Add IsEqualTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/Matcher/IsEqualTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery\Matcher;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+final class IsEqualTest extends MockeryTestCase
+{
+    use MatcherDataProviderTrait;
+
+    /**
+     * @dataProvider isEqualDataProvider
+     */
+    public function testItWorks($expected, $actual): void
+    {
+        self::assertTrue(Mockery::isEqual($expected)->match($actual));
+    }
+}
