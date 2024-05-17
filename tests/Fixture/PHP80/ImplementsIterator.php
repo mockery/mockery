33d2ea8c7fb01daf6ba7a255f9f3f4f0a43760ae
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:10 -0500

    Add ImplementsIterator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/PHP80/ImplementsIterator.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHP80;
+
+class ImplementsIterator implements \Iterator
+{
+    public function rewind(): void
+    {
+    }
+
+    public function current(): mixed
+    {
+    }
+
+    public function key(): mixed
+    {
+    }
+
+    public function next(): void
+    {
+    }
+
+    public function valid(): bool
+    {
+    }
+}
