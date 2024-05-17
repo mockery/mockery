348c88d9f65eb4517d858bcc92dc44b7647fa1a4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:06 -0500

    Add ClassWithStaticMethods.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/PHP73/ClassWithStaticMethods.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PHP73;
+
+class ClassWithStaticMethods
+{
+    public static function foo()
+    {
+        return 'foo';
+    }
+
+    protected static function protectedBar()
+    {
+        return 'bar';
+    }
+}
