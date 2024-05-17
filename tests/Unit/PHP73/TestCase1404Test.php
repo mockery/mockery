8f3ffe8d391fcdc0af6477a9ffbe4d24b338cb29
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:30 -0500

    Add TestCase1404Test.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/PHP73/TestCase1404Test.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\PHP73;
+
+use Generator;
+use Mockery;
+use PDO;
+use PDOStatement;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass Mockery
+ * @requires PHP 7.3
+ * @see https://github.com/mockery/mockery/issues/1404
+ */
+final class TestCase1404Test extends TestCase
+{
+    /**
+     * @return Generator<string,list<string>>
+     */
+    public static function provideResult(): Generator
+    {
+        yield from [
+            'empty' => [[]],
+            'non-empty' => [['Black', 'Lives', 'Matter']],
+        ];
+    }
+
+    /**
+     * @dataProvider provideResult
+     */
+    public function testDemeterChainsAlternativeSyntax(array $result): void
+    {
+        $dbConnection = Mockery::mock(PDO::class);
+
+        $dbConnection->shouldReceive('query->fetchAll')->andReturn($result);
+
+        self::assertSame($result, $dbConnection->query('sql')->fetchAll());
+    }
+
+    /**
+     * @dataProvider provideResult
+     */
+    public function testDemeterChainsExpects(array $result): void
+    {
+        $dbConnection = Mockery::mock(PDO::class);
+
+        $dbConnection->expects('query->fetchAll')->andReturn($result);
+
+        self::assertSame($result, $dbConnection->query('sql')->fetchAll());
+    }
+
+    /**
+     * @dataProvider provideResult
+     */
+    public function testDetestDemeterChainsAllowsmeterChainsAllows(array $result): void
+    {
+        $dbConnection = Mockery::mock(PDO::class);
+
+        $dbConnection->allows('query->fetchAll')->andReturn($result);
+
+        self::assertSame($result, $dbConnection->query('sql')->fetchAll());
+    }
+
+    /**
+     * @dataProvider provideResult
+     */
+    public function testNonDemeterChainsSyntax(array $result): void
+    {
+        $dbStatement = Mockery::mock(PDOStatement::class);
+        $dbStatement->expects('fetchAll')
+            ->andReturn($result);
+
+        $dbConnection = Mockery::mock(PDO::class);
+        $dbConnection->expects('query')
+            ->with('sql')
+            ->andReturn($dbStatement);
+
+        self::assertSame($result, $dbConnection->query('sql')->fetchAll());
+    }
+}
