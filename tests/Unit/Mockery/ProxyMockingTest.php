5e558a281b31fe97e8eccb3a3a2d5078dad14616
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:12 -0500

    Add ProxyMockingTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/ProxyMockingTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery\Exception;
+use PHP73\UnmockableClass;
+
+final class ProxyMockingTest extends MockeryTestCase
+{
+    public function testFinalClassCannotBeMocked(): void
+    {
+        $this->expectException(Exception::class);
+
+        mock(UnmockableClass::class);
+    }
+
+    public function testPassesThruAnyMethod(): void
+    {
+        $mock = mock(new UnmockableClass());
+
+        self::assertSame(1, $mock->anyMethod());
+    }
+
+    public function testPassesThruVirtualMethods(): void
+    {
+        $mock = mock(new UnmockableClass());
+
+        self::assertSame(42, $mock->theAnswer());
+    }
+}
