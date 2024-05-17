2d764cc64b9676ac60e9b8e4141cc09d483418cf
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:27:40 -0500

    Add TestTraversableInterface3.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/PHP73/TestTraversableInterface3.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHP73;
+
+use IteratorAggregate;
+use Traversable;
+
+interface TestTraversableInterface3 extends Traversable, IteratorAggregate
+{
+}
