b0a81ce56255bb0a25fe8ceb8dd51e652ef1cec4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:30 -0500

    Add MockingVoidMethodsTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/PHP73/MockingVoidMethodsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\PHP73;
+
+use PHP73\MethodWithVoidReturnType;
+use Tests\Unit\AbstractTestCase;
+
+final class MockingVoidMethodsTest extends AbstractTestCase
+{
+    public function testItCanStubAndMockVoidMethods(): void
+    {
+        $mock = mock(MethodWithVoidReturnType::class);
+
+        $mock->expects('foo');
+
+        $mock->foo();
+    }
+
+    public function testItShouldSuccessfullyBuildTheMock(): void
+    {
+        self::assertInstanceOf(MethodWithVoidReturnType::class, mock(MethodWithVoidReturnType::class));
+    }
+}
