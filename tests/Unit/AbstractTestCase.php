fe90c4cb0fd9e74062cc68c5fac991588d879474
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:29:30 -0500

    Add AbstractTestCase.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/AbstractTestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+abstract class AbstractTestCase extends MockeryTestCase
+{
+    public function assertInvalidMock(string $class, string $exception, string $message): void
+    {
+        $this->expectException($exception);
+        $this->expectExceptionMessage($message);
+
+        mock($class);
+    }
+
+    public function assertValidMock(string $class): void
+    {
+        self::assertInstanceOf($class, mock($class));
+    }
+}
