44094e4fad935843e8e9e42e2e8c8c07388961a3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:16 -0500

    Add MockeryCanMockMultipleInterfacesWhichOverlapTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/MockeryCanMockMultipleInterfacesWhichOverlapTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery\Container;
+use PHP73\Chatroulette_ConnectionInterface;
+use PHP73\Evenement_EventEmitter;
+
+final class MockeryCanMockMultipleInterfacesWhichOverlapTest extends MockeryTestCase
+{
+    public function testItSshouldNotDuplicateDoublyInheritedMethods(): void
+    {
+        $container = new Container();
+        $mock = $container->mock(
+            Evenement_EventEmitter::class,
+            Chatroulette_ConnectionInterface::class
+        );
+        self::assertInstanceOf(Evenement_EventEmitter::class, $mock);
+    }
+}
