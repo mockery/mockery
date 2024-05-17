e6921c2105a4655f91b8c0022f83dcbe1fb8cc05
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:22 -0500

    Add LoaderTestCase.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/Loader/LoaderTestCase.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery\Loader;
+
+use Mockery\Generator\MockConfiguration;
+use Mockery\Generator\MockDefinition;
+use Mockery\Loader\Loader;
+use PHPUnit\Framework\TestCase;
+
+abstract class LoaderTestCase extends TestCase
+{
+    abstract public function getLoader(): Loader;
+
+    public function testLoad(): void
+    {
+        $className = uniqid('Mock_', false);
+
+        $config = new MockConfiguration([], [], [], $className);
+
+        $code = "<?php class {$className} { } ";
+
+        $definition = new MockDefinition($config, $code);
+
+        $this->getLoader()->load($definition);
+
+        self::assertTrue(class_exists($className));
+    }
+}
