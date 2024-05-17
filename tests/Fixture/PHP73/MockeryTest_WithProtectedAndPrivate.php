7cff0e7dc4d83a96976a3a4499f42248ee6b9ccf
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:27:50 -0500

    Add MockeryTest_WithProtectedAndPrivate.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/PHP73/MockeryTest_WithProtectedAndPrivate.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PHP73;
+
+class MockeryTest_WithProtectedAndPrivate
+{
+    protected function protectedMethod()
+    {
+    }
+
+    private function privateMethod()
+    {
+    }
+}
