021ab0011148f54fbb3efd7a4b299221a8671fac
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:33 -0500

    Add TestCase1402Test.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/PHP74/TestCase1402Test.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\PHP74;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use PHP74\Regression\Issue1402\Service;
+
+/**
+ * @coversDefaultClass Mockery
+ * @requires PHP 7.4
+ * @see https://github.com/mockery/mockery/issues/1402
+ */
+final class TestCase1402Test extends MockeryTestCase
+{
+    public function testMethod(): void
+    {
+        $banana = Mockery::mock(Service::class, [1])->makePartial();
+
+        $banana->allows('test')->andReturns(2);
+
+        self::assertEquals(2, $banana->test());
+    }
+}
