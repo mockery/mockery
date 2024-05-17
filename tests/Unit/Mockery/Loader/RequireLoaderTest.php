c902107236fa4729171a041eb1a3679240e416b7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:22 -0500

    Add RequireLoaderTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/Loader/RequireLoaderTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery\Loader;
+
+use Mockery\Loader\Loader;
+use Mockery\Loader\RequireLoader;
+
+final class RequireLoaderTest extends LoaderTestCase
+{
+    public function getLoader(): Loader
+    {
+        return new RequireLoader(sys_get_temp_dir());
+    }
+}
