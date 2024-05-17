2fee12246ee73d9b576f5dc5485d5d12aed5c65d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-17 03:47:30 -0500

    Update ConstantsPassTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Mockery/Generator/StringManipulation/Pass/ConstantsPassTest.php
+++ tests/Unit/Mockery/Generator/StringManipulation/Pass/ConstantsPassTest.php
@@ -2,14 +2,18 @@
 
 declare(strict_types=1);
 
-namespace Unit\Mockery\Generator\StringManipulation\Pass;
+namespace Tests\Unit\Mockery\Generator\StringManipulation\Pass;
 
 use Mockery\Generator\MockConfiguration;
 use Mockery\Generator\StringManipulation\Pass\ConstantsPass;
 use PHP73\ClassWithConstants;
 use PHPUnit\Framework\TestCase;
+
 use function mb_strpos;
 
+/**
+ * @coversDefaultClass \Mockery
+ */
 final class ConstantsPassTest extends TestCase
 {
     public const CODE = 'class Foo {}';
