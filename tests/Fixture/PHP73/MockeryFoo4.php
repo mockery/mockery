5dc57c8abd3b66a8fdc6fd938b3797c7cb707410
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:03 -0500

    Add MockeryFoo4.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/PHP73/MockeryFoo4.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PHP73;
+
+class MockeryFoo4
+{
+    final public function foo()
+    {
+        return 'baz';
+    }
+
+    public function bar()
+    {
+        return 'bar';
+    }
+}
