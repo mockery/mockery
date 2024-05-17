29a2ce09079f6f47dc67e15bbba8f6a73faf9ef4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:30 -0500

    Add MockingOldStyleConstructorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/PHP73/MockingOldStyleConstructorTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\PHP73;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery\MockInterface;
+use PHP73\OldStyleConstructor;
+
+class MockingOldStyleConstructorTest extends MockeryTestCase
+{
+    public function testMockClassWithOldStyleConstructorAndArguments(): void
+    {
+        $double = mock(OldStyleConstructor::class);
+
+        self::assertInstanceOf(MockInterface::class, $double);
+        self::assertInstanceOf(OldStyleConstructor::class, $double);
+    }
+}
