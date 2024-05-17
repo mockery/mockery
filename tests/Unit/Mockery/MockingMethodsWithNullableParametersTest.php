e547b55cff208eff765272622f94f90c9d5f368e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:14 -0500

    Add MockingMethodsWithNullableParametersTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/MockingMethodsWithNullableParametersTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use PHP73\MethodWithNullableTypedParameter;
+use PHP73\MethodWithParametersWithDefaultValues;
+
+final class MockingMethodsWithNullableParametersTest extends MockeryTestCase
+{
+    public function testItCanHandleDefaultParameters(): void
+    {
+        $mock = mock(MethodWithParametersWithDefaultValues::class);
+
+        self::assertInstanceOf(MethodWithParametersWithDefaultValues::class, $mock);
+    }
+
+    public function testItCanHandleNullableTypedParameters(): void
+    {
+        $mock = mock(MethodWithNullableTypedParameter::class);
+
+        self::assertInstanceOf(MethodWithNullableTypedParameter::class, $mock);
+    }
+}
