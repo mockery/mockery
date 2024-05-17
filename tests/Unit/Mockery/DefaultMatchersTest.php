f2584df98d371ce39989e119d658c6d0a2a94b02
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:27 -0500

    Add DefaultMatchersTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/DefaultMatchersTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery;
+
+use DateTime;
+use Hamcrest\Core\IsEqual;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use PHP73\CustomValueObject;
+use PHP73\CustomValueObjectInterface;
+use PHP73\CustomValueObjectMatcher;
+
+final class DefaultMatchersTest extends MockeryTestCase
+{
+    protected $mock;
+
+    public function mockeryTestSetUp(): void
+    {
+        parent::mockeryTestSetUp();
+        $this->mock = mock('foo');
+    }
+
+    public function mockeryTestTearDown(): void
+    {
+        Mockery::getConfiguration()->allowMockingNonExistentMethods(true);
+        parent::mockeryTestTearDown();
+    }
+
+    public function testDefaultMatcherClass(): void
+    {
+        Mockery::getConfiguration()->setDefaultMatcher(CustomValueObject::class, CustomValueObjectMatcher::class);
+        $this->mock->shouldReceive('foo')->with(new CustomValueObject('expected'))->once();
+        $this->mock->foo(new CustomValueObject('expected'));
+    }
+
+    /**
+     * Just a quickie roundup of a few Hamcrest matchers to check nothing obvious out of place *
+     */
+    public function testDefaultMatcherHamcrest(): void
+    {
+        Mockery::getConfiguration()->setDefaultMatcher(DateTime::class, IsEqual::class);
+        $this->mock->shouldReceive('foo')->with(new DateTime('2000-01-01'))->once();
+        $this->mock->foo(new DateTime('2000-01-01'));
+    }
+
+    public function testDefaultMatcherInterface(): void
+    {
+        Mockery::getConfiguration()->setDefaultMatcher(CustomValueObjectInterface::class, CustomValueObjectMatcher::class);
+        $this->mock->shouldReceive('foo')->with(new CustomValueObject('expected2'))->once();
+        $this->mock->foo(new CustomValueObject('expected2'));
+    }
+}
