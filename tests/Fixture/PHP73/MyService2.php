ba0e1d38c70fab446b1a0390c22902f21c018cb3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:27:47 -0500

    Add MyService2.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/PHP73/MyService2.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHP73;
+
+class MyService2
+{
+    public function login($user, $pass)
+    {
+    }
+
+    public function hasBookmarksTagged($tag)
+    {
+    }
+
+    public function addBookmark($uri, $tag)
+    {
+    }
+}
