dc896be9e39426c9541b03d9c020edf3cc0d7700
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:15 -0500

    Add MockingClassConstantsTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/MockingClassConstantsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use PHP73\ClassWithConstants;
+
+final class MockingClassConstantsTest extends MockeryTestCase
+{
+    public function testItShouldAllowToMockClassConstants(): void
+    {
+        Mockery::getConfiguration()->setConstantsMap([
+            ClassWithConstants::class => [
+                'FOO' => 'baz',
+                'X' => 2,
+                'BAZ' => [
+                    'qux' => 'daz',
+                ],
+            ],
+        ]);
+
+        $mock = Mockery::mock('overload:' . ClassWithConstants::class);
+
+        self::assertEquals('baz', $mock::FOO);
+        self::assertEquals(2, $mock::X);
+        self::assertEquals([
+            'qux' => 'daz',
+        ], $mock::BAZ);
+    }
+}
