780e246e3c5bce04ef25c601c6914cc44c05cb88
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:26 -0500

    Add MockConfigurationBuilderTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/Generator/MockConfigurationBuilderTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery\Generator;
+
+use Mockery\Generator\MockConfigurationBuilder;
+use PHP73\ClassWithDebugInfo;
+use PHP73\ClassWithMagicCall;
+use PHPUnit\Framework\TestCase;
+
+final class MockConfigurationBuilderTest extends TestCase
+{
+    public function testMagicMethodsAreBlackListedByDefault(): void
+    {
+        $builder = new MockConfigurationBuilder();
+        $builder->addTarget(ClassWithMagicCall::class);
+        $methods = $builder->getMockConfiguration()->getMethodsToMock();
+        self::assertCount(1, $methods);
+        self::assertEquals('foo', $methods[0]->getName());
+    }
+
+    public function testReservedWordsAreBlackListedByDefault(): void
+    {
+        $builder = new MockConfigurationBuilder();
+
+        self::assertContains('__halt_compiler', $builder->getMockConfiguration()->getBlackListedMethods());
+
+        // need a builtin for this
+        self::markTestSkipped('Need a builtin class with a method that is a reserved word');
+    }
+
+    public function testXDebugsDebugInfoIsBlackListedByDefault(): void
+    {
+        $builder = new MockConfigurationBuilder();
+        $builder->addTarget(ClassWithDebugInfo::class);
+        $methods = $builder->getMockConfiguration()->getMethodsToMock();
+        self::assertCount(1, $methods);
+        self::assertEquals('foo', $methods[0]->getName());
+    }
+}
