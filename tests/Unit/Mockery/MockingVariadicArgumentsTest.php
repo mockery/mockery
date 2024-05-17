df24949db01ec6ce795f1a8eb30169c5cef4d50b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:13 -0500

    Add MockingVariadicArgumentsTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/MockingVariadicArgumentsTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use PHP73\TestWithVariadicArguments;
+
+final class MockingVariadicArgumentsTest extends MockeryTestCase
+{
+    public function testShouldAllowMockingVariadicArguments(): void
+    {
+        $mock = mock(TestWithVariadicArguments::class);
+
+        $mock->shouldReceive('foo')->andReturn('notbar');
+        self::assertEquals('notbar', $mock->foo());
+    }
+}
