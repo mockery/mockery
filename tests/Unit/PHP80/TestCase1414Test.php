400e16c55bc8f4ebffa534021dc24b2055dc1636
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:29:27 -0500

    Add TestCase1414Test.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/PHP80/TestCase1414Test.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\PHP80;
+
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+/**
+ * @coversDefaultClass Mockery
+ * @requires PHP 8.0
+ * @see https://github.com/mockery/mockery/issues/1414
+ */
+final class TestCase1414Test extends TestCase
+{
+    public function testMockAnonymousClass(): void
+    {
+        $class = new class() extends stdClass {
+        };
+
+        $mock = Mockery::mock($class::class);
+
+        self::assertInstanceOf($class::class, $mock);
+    }
+}
