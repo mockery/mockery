bb88021427710debe48779f299bebba44c152a78
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-17 03:47:26 -0500

    Update MockClassWithIterableReturnTypeTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Mockery/MockClassWithIterableReturnTypeTest.php
+++ tests/Unit/Mockery/MockClassWithIterableReturnTypeTest.php
@@ -7,6 +7,11 @@ namespace Tests\Unit\Mockery;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 use PHP73\ReturnTypeIterableTypeHint;
 
+use function mock;
+
+/**
+ * @coversDefaultClass \Mockery
+ */
 final class MockClassWithIterableReturnTypeTest extends MockeryTestCase
 {
     public function testMockingIterableReturnType(): void
