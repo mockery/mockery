a375c843cd8a75b5c610c03847156e492d4f6ad1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:46 -0500

    Update Bootstrap.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,25 +1,6 @@
 <?php
 
 declare(strict_types=1);
-/**
- * Mockery
- *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://github.com/padraic/mockery/master/LICENSE
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to padraic@php.net so we can send you a copy immediately.
- *
- * @category   Mockery
- * @package    Mockery
- * @subpackage UnitTests
- * @copyright  Copyright (c) 2010 Pádraic Brady (http://blog.astrumfutura.com)
- * @license    http://github.com/padraic/mockery/blob/master/LICENSE New BSD License
- */
 
 /*
  * Set error reporting to the level to which Mockery code must comply.
