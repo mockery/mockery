0fb97523cd64ec7d76bc9abf512b981da4182d9e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:34 -0500

    Add Php84LanguageFeaturesTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/PHP84/Php84LanguageFeaturesTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\PHP84;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use stdClass;
+
+/**
+ * @requires PHP 8.4.0-dev
+ */
+final class Php84LanguageFeaturesTest extends MockeryTestCase
+{
+    public function testExample(): void
+    {
+        self::assertInstanceOf(
+            stdClass::class,
+            Mockery::mock(stdClass::class)
+        );
+    }
+}
