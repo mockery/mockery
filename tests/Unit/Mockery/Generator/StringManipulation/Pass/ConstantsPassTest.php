5fbbfde6de1812565626b571eaac73fcfd9b180b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:25 -0500

    Add ConstantsPassTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/Generator/StringManipulation/Pass/ConstantsPassTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\Mockery\Generator\StringManipulation\Pass;
+
+use Mockery\Generator\MockConfiguration;
+use Mockery\Generator\StringManipulation\Pass\ConstantsPass;
+use PHP73\ClassWithConstants;
+use PHPUnit\Framework\TestCase;
+use function mb_strpos;
+
+final class ConstantsPassTest extends TestCase
+{
+    public const CODE = 'class Foo {}';
+
+    public function testShouldAddConstants(): void
+    {
+        $pass = new ConstantsPass();
+
+        $config = new MockConfiguration(
+            [],
+            [],
+            [],
+            ClassWithConstants::class,
+            false,
+            [],
+            false,
+            [
+                ClassWithConstants::class => [
+                    'FOO' => 'test',
+                ],
+            ]
+        );
+
+        $code = $pass->apply(static::CODE, $config);
+
+        self::assertNotFalse(mb_strpos($code, "const FOO = 'test'"));
+    }
+}
