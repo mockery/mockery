67d91e48e0a4a5ece8a4e03e48de5b4a87b30ead
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:26 -0500

    Add CallTypeHintPassTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/Generator/StringManipulation/Pass/CallTypeHintPassTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\Mockery\Generator\StringManipulation\Pass;
+
+use Mockery;
+use Mockery\Generator\MockConfiguration;
+use Mockery\Generator\StringManipulation\Pass\CallTypeHintPass;
+use PHPUnit\Framework\TestCase;
+use function mb_strpos;
+
+final class CallTypeHintPassTest extends TestCase
+{
+    public const CODE = ' public function __call($method, array $args) {}
+                   public static function __callStatic($method, array $args) {}
+    ';
+
+    public function testShouldRemoveCallStaticTypeHintIfRequired(): void
+    {
+        $pass = new CallTypeHintPass();
+        $config = Mockery::mock(MockConfiguration::class, [
+            'requiresCallStaticTypeHintRemoval' => true,
+        ])->makePartial();
+        $code = $pass->apply(self::CODE, $config);
+        self::assertNotFalse(mb_strpos($code, '__callStatic($method, $args)'));
+    }
+
+    public function testShouldRemoveCallTypeHintIfRequired(): void
+    {
+        $pass = new CallTypeHintPass();
+        $config = Mockery::mock(MockConfiguration::class, [
+            'requiresCallTypeHintRemoval' => true,
+        ])->makePartial();
+        $code = $pass->apply(self::CODE, $config);
+        self::assertNotFalse(mb_strpos($code, '__call($method, $args)'));
+    }
+}
