c22fbf5bae9459e9a454894d1b28f493f647d761
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:29 -0500

    Add MockeryPHPUnitIntegrationTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/Adapter/Phpunit/MockeryPHPUnitIntegrationTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery\Adapter\Phpunit;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery\Exception\BadMethodCallException;
+use PHP73\BaseClassStub;
+use Throwable;
+
+final class MockeryPHPUnitIntegrationTest extends MockeryTestCase
+{
+    public function testItMarksAPassingTestAsRiskyIfWeThrewExceptions(): void
+    {
+        $mock = mock();
+
+        try {
+            $mock->foobar();
+        } catch (Throwable $e) {
+            // exception swallowed...
+        }
+
+        $test = spy(BaseClassStub::class)->makePartial();
+        $test->finish();
+
+        $test->shouldHaveReceived()->markAsRisky();
+    }
+
+    public function testTheUserCanManuallyDismissAnExceptionToAvoidTheRiskyTest(): void
+    {
+        $mock = mock();
+
+        try {
+            $mock->foobar();
+        } catch (BadMethodCallException $e) {
+            $e->dismiss();
+        }
+
+        $test = spy(BaseClassStub::class)->makePartial();
+        $test->finish();
+
+        $test->shouldNotHaveReceived()->markAsRisky();
+    }
+}
