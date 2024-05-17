b2117a6d7d875aee371f22fe381db5de2c227960
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:01 -0500

    Add MockeryTest_ClassConstructor2.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/PHP73/MockeryTest_ClassConstructor2.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PHP73;
+
+class MockeryTest_ClassConstructor2
+{
+    protected $param1;
+
+    public function __construct(\stdClass $param1)
+    {
+        $this->param1 = $param1;
+    }
+
+    public function getParam1()
+    {
+        return $this->param1;
+    }
+
+    public function foo()
+    {
+        return 'foo';
+    }
+
+    public function bar()
+    {
+        return $this->foo();
+    }
+}
