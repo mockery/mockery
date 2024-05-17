3277de7eae0e10f84451faf2b5e02446c723db52
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-17 09:59:51 -0500

    Update autoload.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Fixture/autoload.php
+++ tests/Fixture/autoload.php
@@ -2,35 +2,22 @@
 
 declare(strict_types=1);
 
-spl_autoload_register(static function (string $class): void {
-    $directory = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-
-    $filePath = sprintf(
-        '%s%s%s.php',
-        __DIR__,
-        DIRECTORY_SEPARATOR,
-        $directory
-    );
-
-    if (! file_exists($filePath)) {
-        $filePath = sprintf(
-            '%s%sNamespaced%s%s.php',
-            __DIR__,
-            DIRECTORY_SEPARATOR,
-            DIRECTORY_SEPARATOR,
-            $directory
-        );
-
-        if (! file_exists($filePath)) {
-            return;
-        }
-    }
-
-    require_once $filePath;
-});
+use Composer\Autoload\ClassLoader;
 
 $rootDir = dirname(__DIR__, 2);
+
 $loader = require $rootDir . '/vendor/autoload.php';
+
+if (! $loader instanceof ClassLoader){
+    throw new \RuntimeException('Unable to load ' . ClassLoader::class);
+}
+
+$loader->add('', $rootDir . '/tests/Fixture/Namespaced');
+$loader->addPsr4('PHP73\\', $rootDir . '/tests/Fixture/PHP73');
+$loader->addPsr4('PHP74\\', $rootDir . '/tests/Fixture/PHP74');
+$loader->addPsr4('PHP80\\', $rootDir . '/tests/Fixture/PHP80');
+$loader->addPsr4('PHP81\\', $rootDir . '/tests/Fixture/PHP81');
+$loader->addPsr4('PHP82\\', $rootDir . '/tests/Fixture/PHP82');
+$loader->addPsr4('PHP83\\', $rootDir . '/tests/Fixture/PHP83');
+$loader->addPsr4('PHP84\\', $rootDir . '/tests/Fixture/PHP84');
 $loader->addPsr4('Tests\\', $rootDir . '/tests');
-//$loader->addPsr4('Mockery\\Tests\\', $rootDir . '/tests');
-//$loader->addPsr4('test\\', $rootDir . '/tests');
