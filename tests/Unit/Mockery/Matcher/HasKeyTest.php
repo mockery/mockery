14a643c91b8eb0f083b1d2dc5778359562dd33bd
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:22 -0500

    Add HasKeyTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/Matcher/HasKeyTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery\Matcher;
+
+use ArrayObject;
+use Mockery\Matcher\HasKey;
+use PHPUnit\Framework\TestCase;
+
+final class HasKeyTest extends TestCase
+{
+    public function testItCanHandleANonArray(): void
+    {
+        $matcher = new HasKey('dave');
+
+        $actual = null;
+
+        self::assertFalse($matcher->match($actual));
+    }
+
+    public function testItMatchesAnArray(): void
+    {
+        $matcher = new HasKey('dave');
+
+        $actual = [
+            'foo' => 'bar',
+            'dave' => 123,
+            'bar' => 'baz',
+        ];
+
+        self::assertTrue($matcher->match($actual));
+    }
+
+    public function testItMatchesAnArrayLikeObject(): void
+    {
+        $matcher = new HasKey('dave');
+
+        $actual = new ArrayObject([
+            'foo' => 'bar',
+            'dave' => 123,
+            'bar' => 'baz',
+        ]);
+
+        self::assertTrue($matcher->match($actual));
+    }
+}
