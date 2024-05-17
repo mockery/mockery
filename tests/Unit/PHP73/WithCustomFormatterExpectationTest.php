02e53e1f4b0fcfbe094ecddf2e011227507fc9d3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:30 -0500

    Add WithCustomFormatterExpectationTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/PHP73/WithCustomFormatterExpectationTest.php
@@ -0,0 +1,146 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\PHP73;
+
+use Mockery;
+use PHP73\ClassChildOfWithCustomFormatter;
+use PHP73\ClassImplementsWithCustomFormatter;
+use PHP73\ClassWithCustomFormatter;
+use PHP73\ClassWithoutCustomFormatter;
+use PHP73\InterfaceWithCustomFormatter;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+final class WithCustomFormatterExpectationTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        Mockery::getConfiguration()->setObjectFormatter(
+            ClassWithCustomFormatter::class,
+            function ($object, $nesting) {
+                return [
+                    'formatter' => ClassWithCustomFormatter::class,
+                    'properties' => [
+                        'stringProperty' => $object->stringProperty,
+                    ],
+                    'getters' => [
+                        'gettedProperty' => $object->getArrayProperty(),
+                    ],
+                ];
+            }
+        );
+        Mockery::getConfiguration()->setObjectFormatter(
+            InterfaceWithCustomFormatter::class,
+            function ($object, $nesting) {
+                return [
+                    'formatter' => InterfaceWithCustomFormatter::class,
+                    'properties' => [
+                        'stringProperty' => $object->stringProperty,
+                    ],
+                    'getters' => [
+                        'gettedProperty' => $object->getArrayProperty(),
+                    ],
+                ];
+            }
+        );
+    }
+
+    public static function formatObjectsDataProvider(): array
+    {
+        return [
+            [
+                new ClassWithoutCustomFormatter(),
+                [
+                    'stringProperty',
+                    'numberProperty',
+                    'arrayProperty',
+                ],
+                [
+                    'privateProperty',
+                ],
+            ],
+            [
+                new ClassWithCustomFormatter(),
+                [
+                    'stringProperty',
+                    'gettedProperty',
+                ],
+                [
+                    'numberProperty',
+                    'privateProperty',
+                ],
+            ],
+            [
+                new ClassImplementsWithCustomFormatter(),
+                [
+                    'stringProperty',
+                    'gettedProperty',
+                ],
+                [
+                    'numberProperty',
+                    'privateProperty',
+                ],
+            ],
+        ];
+    }
+
+    public static function getObjectFormatterDataProvider(): array
+    {
+        return [
+            [
+                new stdClass(),
+                null,
+            ],
+            [
+                new ClassWithoutCustomFormatter(),
+                null,
+            ],
+            [
+                new ClassWithCustomFormatter(),
+                ClassWithCustomFormatter::class,
+            ],
+            [
+                new ClassChildOfWithCustomFormatter(),
+                ClassWithCustomFormatter::class,
+            ],
+            [
+                new ClassImplementsWithCustomFormatter(),
+                InterfaceWithCustomFormatter::class,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider formatObjectsDataProvider
+     */
+    public function testFormatObjects($obj, $shouldContains, $shouldNotContains): void
+    {
+        $string = Mockery::formatObjects([$obj]);
+        foreach ($shouldContains as $containString) {
+            self::assertStringContainsString($containString, $string);
+        }
+        foreach ($shouldNotContains as $containString) {
+            self::assertStringNotContainsString($containString, $string);
+        }
+    }
+
+    /**
+     * @dataProvider getObjectFormatterDataProvider
+     */
+    public function testGetObjectFormatter($object, $expected): void
+    {
+        $defaultFormatter = function ($class, $nesting) {
+            return null;
+        };
+
+        $formatter = Mockery::getConfiguration()->getObjectFormatter(get_class($object), $defaultFormatter);
+        $formatted = $formatter($object, 1);
+
+        self::assertEquals(
+            $expected,
+            $formatted ? $formatted['formatter'] : null
+        );
+    }
+}
