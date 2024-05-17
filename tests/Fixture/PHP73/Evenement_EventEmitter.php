4f51a69875d2a1e7fc26fd1902523347094f4b0a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:05 -0500

    Add Evenement_EventEmitter.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/PHP73/Evenement_EventEmitter.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHP73;
+
+class Evenement_EventEmitter implements Evenement_EventEmitterInterface
+{
+    public function on($name, $callback)
+    {
+    }
+}
