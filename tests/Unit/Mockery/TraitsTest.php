ac2f65a390759931ca88b8a4bf3f58848bd79d8a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:11 -0500

    Add TraitsTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/TraitsTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use PHP73\SimpleTrait;
+use PHP73\TraitWithAbstractMethod;
+
+final class TraitsTest extends MockeryTestCase
+{
+    public function testItCanCreateAnObjectForASimpleTrait(): void
+    {
+        $trait = mock(SimpleTrait::class);
+
+        self::assertEquals('bar', $trait->foo());
+    }
+
+    public function testItCanCreateAnObjectUsingMultipleTraits(): void
+    {
+        $trait = mock(SimpleTrait::class, TraitWithAbstractMethod::class, [
+            'doBaz' => 123,
+        ]);
+
+        self::assertEquals('bar', $trait->foo());
+        self::assertEquals(123, $trait->baz());
+    }
+
+    public function testItCreatesAbstractMethodsAsNecessary(): void
+    {
+        $trait = mock(TraitWithAbstractMethod::class, [
+            'doBaz' => 'baz',
+        ]);
+
+        self::assertEquals('baz', $trait->baz());
+    }
+}
