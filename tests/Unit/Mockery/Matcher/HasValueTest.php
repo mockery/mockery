f339f5cc349e268311a970d3ffeea26e69c8e211
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:21 -0500

    Add HasValueTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/Matcher/HasValueTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery\Matcher;
+
+use ArrayObject;
+use Mockery\Matcher\HasValue;
+use PHPUnit\Framework\TestCase;
+
+final class HasValueTest extends TestCase
+{
+    public function testItCanHandleANonArray(): void
+    {
+        $matcher = new HasValue(123);
+
+        $actual = null;
+
+        self::assertFalse($matcher->match($actual));
+    }
+
+    public function testItMatchesAnArray(): void
+    {
+        $matcher = new HasValue(123);
+
+        $actual = [
+            'foo' => 'bar',
+            'dave' => 123,
+            'bar' => 'baz',
+        ];
+
+        self::assertTrue($matcher->match($actual));
+    }
+
+    public function testItMatchesAnArrayLikeObject(): void
+    {
+        $matcher = new HasValue(123);
+
+        $actual = new ArrayObject([
+            'foo' => 'bar',
+            'dave' => 123,
+            'bar' => 'baz',
+        ]);
+
+        self::assertTrue($matcher->match($actual));
+    }
+}
