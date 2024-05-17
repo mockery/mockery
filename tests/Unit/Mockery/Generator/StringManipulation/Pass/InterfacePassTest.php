3ead4c25b56a292a773f8abb39ed18140849bba8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:24 -0500

    Add InterfacePassTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/Generator/StringManipulation/Pass/InterfacePassTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\Mockery\Generator\StringManipulation\Pass;
+
+use Mockery;
+use Mockery\Generator\MockConfiguration;
+use Mockery\Generator\StringManipulation\Pass\InterfacePass;
+use PHPUnit\Framework\TestCase;
+use function mb_strpos;
+
+final class InterfacePassTest extends TestCase
+{
+    public const CODE = 'class Mock implements MockInterface';
+
+    public function testShouldAddAnyInterfaceNamesToImplementsDefinition(): void
+    {
+        $pass = new InterfacePass();
+
+        $config = Mockery::mock(MockConfiguration::class, [
+            'getTargetInterfaces' => [
+                Mockery::mock([
+                    'getName' => '\Dave\Dave',
+                ]),
+                Mockery::mock([
+                    'getName' => '\Paddy\Paddy',
+                ]),
+            ],
+        ]);
+
+        $code = $pass->apply(self::CODE, $config);
+
+        self::assertNotFalse(mb_strpos($code, 'implements MockInterface, \Dave\Dave, \Paddy\Paddy'));
+    }
+
+    public function testShouldNotAlterCodeIfNoTargetInterfaces(): void
+    {
+        $pass = new InterfacePass();
+
+        $config = Mockery::mock(MockConfiguration::class, [
+            'getTargetInterfaces' => [],
+        ]);
+
+        $code = $pass->apply(self::CODE, $config);
+        self::assertEquals(self::CODE, $code);
+    }
+}
