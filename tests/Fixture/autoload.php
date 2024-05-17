e4034e9c7f3066c1b17dbb47d7afce6bcf92b612
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:29:32 -0500

    Add autoload.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/autoload.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+spl_autoload_register(static function (string $class): void {
+    $directory = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+
+    $filePath = sprintf(
+        '%s%s%s.php',
+        __DIR__,
+        DIRECTORY_SEPARATOR,
+        $directory
+    );
+
+    if (! file_exists($filePath)) {
+        $filePath = sprintf(
+            '%s%sNamespaced%s%s.php',
+            __DIR__,
+            DIRECTORY_SEPARATOR,
+            DIRECTORY_SEPARATOR,
+            $directory
+        );
+
+        if (! file_exists($filePath)) {
+            return;
+        }
+    }
+
+    require_once $filePath;
+});
+
+$rootDir = dirname(__DIR__, 2);
+$loader = require $rootDir . '/vendor/autoload.php';
+//$loader->addPsr4('Tests\\', $rootDir . '/tests');
+//$loader->addPsr4('Mockery\\Tests\\', $rootDir . '/tests');
+//$loader->addPsr4('test\\', $rootDir . '/tests');
