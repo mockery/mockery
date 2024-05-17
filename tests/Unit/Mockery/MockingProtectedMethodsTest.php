e205273d3c25d34e80921c957c2c4770e5cfe8be
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:14 -0500

    Add MockingProtectedMethodsTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/MockingProtectedMethodsTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use PHP73\TestIncreasedVisibilityChild;
+use PHP73\TestWithProtectedMethods;
+
+final class MockingProtectedMethodsTest extends MockeryTestCase
+{
+    public function testShouldAllowMockingAbstractProtectedMethods(): void
+    {
+        $mock = mock(TestWithProtectedMethods::class)
+            ->makePartial()
+            ->shouldAllowMockingProtectedMethods();
+
+        $mock->shouldReceive('abstractProtected')->andReturn('abstractProtected');
+        self::assertEquals('abstractProtected', $mock->foo());
+    }
+
+    public function testShouldAllowMockingIncreasedVisabilityMethods(): void
+    {
+        $mock = mock(TestIncreasedVisibilityChild::class);
+        $mock->shouldReceive('foobar')->andReturn('foobar');
+        self::assertEquals('foobar', $mock->foobar());
+    }
+
+    public function testShouldAllowMockingProtectedMethodOnDefinitionTimePartial(): void
+    {
+        $mock = mock(TestWithProtectedMethods::class . '[protectedBar]')
+            ->shouldAllowMockingProtectedMethods();
+
+        $mock->shouldReceive('protectedBar')->andReturn('notbar');
+        self::assertEquals('notbar', $mock->bar());
+    }
+
+    public function testShouldAllowMockingProtectedMethods(): void
+    {
+        $mock = mock(TestWithProtectedMethods::class)
+            ->makePartial()
+            ->shouldAllowMockingProtectedMethods();
+
+        $mock->shouldReceive('protectedBar')->andReturn('notbar');
+        self::assertEquals('notbar', $mock->bar());
+    }
+
+    /**
+     * This is a regression test, basically we don't want the mock handling
+     * interfering with calling protected methods partials
+     */
+    public function testShouldAutomaticallyDeferCallsToProtectedMethodsForPartials(): void
+    {
+        $mock = mock(TestWithProtectedMethods::class . '[foo]');
+
+        self::assertEquals('bar', $mock->bar());
+    }
+
+    /**
+     * This is a regression test, basically we don't want the mock handling
+     * interfering with calling protected methods partials
+     */
+    public function testShouldAutomaticallyDeferCallsToProtectedMethodsForRuntimePartials(): void
+    {
+        $mock = mock(TestWithProtectedMethods::class)->makePartial();
+        self::assertEquals('bar', $mock->bar());
+    }
+
+    public function testShouldAutomaticallyIgnoreAbstractProtectedMethods(): void
+    {
+        $mock = mock(TestWithProtectedMethods::class)->makePartial();
+        self::assertNull($mock->foo());
+    }
+}
