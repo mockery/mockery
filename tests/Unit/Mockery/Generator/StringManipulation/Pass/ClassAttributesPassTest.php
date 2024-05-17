c927ab1038990b8aab83d23f33848ebaebe9fc2b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-17 03:47:34 -0500

    Update ClassAttributesPassTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Mockery/Generator/StringManipulation/Pass/ClassAttributesPassTest.php
+++ tests/Unit/Mockery/Generator/StringManipulation/Pass/ClassAttributesPassTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Unit\Mockery\Generator\StringManipulation\Pass;
+namespace Tests\Unit\Mockery\Generator\StringManipulation\Pass;
 
 use Generator;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
@@ -10,6 +10,12 @@ use Mockery\Generator\MockConfiguration;
 use Mockery\Generator\StringManipulation\Pass\ClassAttributesPass;
 use Mockery\Generator\UndefinedTargetClass;
 
+use function file_get_contents;
+use function mock;
+
+/**
+ * @coversDefaultClass \Mockery
+ */
 class ClassAttributesPassTest extends MockeryTestCase
 {
     public const CODE = 'namespace Mockery; class Mock {}';
@@ -25,18 +31,12 @@ class ClassAttributesPassTest extends MockeryTestCase
         ];
 
         yield 'has one attribute' => [
-            'attributes' => [
-                'Attribute1',
-            ],
+            'attributes' => ['Attribute1'],
             'expected' => '#[Attribute1]',
         ];
 
         yield 'has attributes' => [
-            'attributes' => [
-                'Attribute1',
-                'Attribute2',
-                'Attribute3()',
-            ],
+            'attributes' => ['Attribute1', 'Attribute2', 'Attribute3()'],
             'expected' => '#[Attribute1,Attribute2,Attribute3()]',
         ];
     }
@@ -44,10 +44,8 @@ class ClassAttributesPassTest extends MockeryTestCase
     /**
      * @dataProvider providerCanApplyClassAttributes
      */
-    public function testCanApplyClassAttributes(
-        array $attributes,
-        string $expected
-    ): void {
+    public function testCanApplyClassAttributes(array $attributes, string $expected): void
+    {
         $undefinedTargetClass = mock(UndefinedTargetClass::class);
         $undefinedTargetClass->expects('getAttributes')
             ->once()
