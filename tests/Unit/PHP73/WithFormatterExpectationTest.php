5d30d95e1cc6f7ca8d17026e423f9a06eb265adc
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:29 -0500

    Add WithFormatterExpectationTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/PHP73/WithFormatterExpectationTest.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\PHP73;
+
+use Mockery;
+use Mockery\Exception\NoMatchingExpectationException;
+use PHP73\ClassWithGetter;
+use PHP73\ClassWithGetterWithParam;
+use PHP73\ClassWithPublicStaticGetter;
+use PHP73\ClassWithPublicStaticProperty;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+use function mb_strpos;
+
+final class WithFormatterExpectationTest extends TestCase
+{
+    public static function formatObjectsDataProvider(): array
+    {
+        return [
+            [
+                [null],
+                '',
+            ],
+            [
+                ['a string', 98768, ['a', 'nother', 'array']],
+                '',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider formatObjectsDataProvider
+     */
+    public function testFormatObjects($args, $expected): void
+    {
+        self::assertEquals(
+            $expected,
+            Mockery::formatObjects($args)
+        );
+    }
+
+    public function testFormatObjectsExcludesStaticGetters(): void
+    {
+        $obj = new ClassWithPublicStaticGetter();
+        $string = Mockery::formatObjects([$obj]);
+
+        self::assertSame(mb_strpos($string, 'getExcluded'), false);
+    }
+
+    public function testFormatObjectsExcludesStaticProperties(): void
+    {
+        $obj = new ClassWithPublicStaticProperty();
+        $string = Mockery::formatObjects([$obj]);
+
+        self::assertSame(mb_strpos($string, 'excludedProperty'), false);
+    }
+
+    public function testFormatObjectsShouldNotCallGettersWithParams(): void
+    {
+        $obj = new ClassWithGetterWithParam();
+        $string = Mockery::formatObjects([$obj]);
+
+        self::assertSame(mb_strpos($string, 'Missing argument 1 for'), false);
+    }
+
+    /**
+     * Note that without the patch checked in with this test, rather than throwing
+     * an exception, the program will go into an infinite recursive loop
+     */
+    public function testFormatObjectsWithMockCalledInGetterDoesNotLeadToRecursion(): void
+    {
+        $mock = Mockery::mock(stdClass::class);
+        $mock->shouldReceive('doBar')->with('foo');
+        $obj = new ClassWithGetter($mock);
+        $this->expectException(NoMatchingExpectationException::class);
+        $obj->getFoo();
+    }
+}
