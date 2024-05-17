9a169954c5a62260de671f7d66326cbdf1e84147
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:18 -0500

    Add MockClassWithUnknownTypeHintTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/MockClassWithUnknownTypeHintTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery\MockInterface;
+use PHP73\HasUnknownClassAsTypeHintOnMethod;
+
+final class MockClassWithUnknownTypeHintTest extends MockeryTestCase
+{
+    public function testItShouldSuccessfullyBuildTheMock(): void
+    {
+        $mock = mock(HasUnknownClassAsTypeHintOnMethod::class);
+
+        self::assertInstanceOf(MockInterface::class, $mock);
+    }
+}
