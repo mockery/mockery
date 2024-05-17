015b21de88103085e69eea3b56c8702c3f32c7c1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:02 -0500

    Add MockeryTestFoo2.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/PHP73/MockeryTestFoo2.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PHP73;
+
+#[\AllowDynamicProperties]
+class MockeryTestFoo2
+{
+    public function foo()
+    {
+        return 'foo';
+    }
+
+    public function bar()
+    {
+        return 'bar';
+    }
+}
