215ce89e531cc70e919b2b4e5fa9ab515a236a2d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:16 -0500

    Add MockeryCanMockClassesWithSemiReservedWordsTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/MockeryCanMockClassesWithSemiReservedWordsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery;
+
+use Mockery;
+use PHP73\SemiReservedWordsAsMethods;
+use PHPUnit\Framework\TestCase;
+
+final class MockeryCanMockClassesWithSemiReservedWordsTest extends TestCase
+{
+    public function testMockSemiReservedWordsAsMethods(): void
+    {
+        $mock = Mockery::mock(SemiReservedWordsAsMethods::class);
+
+        $mock->shouldReceive('include')->andReturn('foo');
+
+        self::assertTrue(method_exists($mock, 'include'));
+        self::assertEquals('foo', $mock->include());
+    }
+}
