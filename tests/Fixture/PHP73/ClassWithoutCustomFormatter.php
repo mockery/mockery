180a5e4ff2a36eb078741b01393a5e775fe4368a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:05 -0500

    Add ClassWithoutCustomFormatter.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/PHP73/ClassWithoutCustomFormatter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PHP73;
+
+class ClassWithoutCustomFormatter
+{
+    public $stringProperty = 'a string';
+    public $numberProperty = 123;
+    public $arrayProperty = ['a', 'nother', 'array'];
+    private $privateProperty = 'private';
+}
