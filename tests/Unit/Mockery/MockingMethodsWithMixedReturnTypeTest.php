a614442b86a0a48f786e3cf78fd5ea9e963c02a8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:15 -0500

    Add MockingMethodsWithMixedReturnTypeTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/MockingMethodsWithMixedReturnTypeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use PHP73\MyInterface;
+
+class MockingMethodsWithMixedReturnTypeTest extends MockeryTestCase
+{
+    public function testMockingMixedReturnType(): void
+    {
+        $mock = Mockery::mock(MyInterface::class);
+
+        $mock->shouldReceive('foo->bar')->andReturn('bar');
+
+        self::assertSame('bar', $mock->foo()->bar());
+    }
+}
