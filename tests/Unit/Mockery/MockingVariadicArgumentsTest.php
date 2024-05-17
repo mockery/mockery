5c6289df3ac5ebd57a3566f67a4761e793f22baa
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-17 03:47:23 -0500

    Update MockingVariadicArgumentsTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Mockery/MockingVariadicArgumentsTest.php
+++ tests/Unit/Mockery/MockingVariadicArgumentsTest.php
@@ -7,13 +7,19 @@ namespace Tests\Unit\Mockery;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 use PHP73\TestWithVariadicArguments;
 
+use function mock;
+
+/**
+ * @coversDefaultClass \Mockery
+ */
 final class MockingVariadicArgumentsTest extends MockeryTestCase
 {
     public function testShouldAllowMockingVariadicArguments(): void
     {
         $mock = mock(TestWithVariadicArguments::class);
 
-        $mock->shouldReceive('foo')->andReturn('notbar');
+        $mock->shouldReceive('foo')
+            ->andReturn('notbar');
         self::assertEquals('notbar', $mock->foo());
     }
 }
