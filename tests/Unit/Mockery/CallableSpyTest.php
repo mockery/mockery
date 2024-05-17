28693e2cb366b40f26944a5281c0d96634ab6731
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:28 -0500

    Add CallableSpyTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/CallableSpyTest.php
@@ -0,0 +1,183 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery\Exception\InvalidCountException;
+
+final class CallableSpyTest extends MockeryTestCase
+{
+    public function testItActsAsPartial(): void
+    {
+        $spy = spy(function ($number) {
+            return $number + 1;
+        });
+
+        self::assertEquals(124, $spy(123));
+        $spy->shouldHaveBeenCalled();
+    }
+
+    public function testItCanVerifyItWasCalledANumberOfTimes(): void
+    {
+        $spy = spy(function () {
+        });
+
+        $spy();
+        $spy();
+
+        $spy->shouldHaveBeenCalled()->twice();
+    }
+
+    public function testItCanVerifyItWasCalledANumberOfTimesWithParticularArguments(): void
+    {
+        $spy = spy(function () {
+        });
+
+        $spy(123);
+        $spy(123);
+
+        $spy->shouldHaveBeenCalled()->with(123)->twice();
+    }
+
+    public function testItThrowsIfItWasCalledLessThanTheNumberOfTimesWeExpected(): void
+    {
+        $spy = spy(function () {
+        });
+
+        $spy();
+
+        $this->expectException(InvalidCountException::class);
+        $spy->shouldHaveBeenCalled()->twice();
+    }
+
+    public function testItThrowsIfItWasCalledLessThanTheNumberOfTimesWeExpectedWithParticularArguments(): void
+    {
+        $spy = spy(function () {
+        });
+
+        $spy();
+        $spy(123);
+
+        $this->expectException(InvalidCountException::class);
+        $spy->shouldHaveBeenCalled()->with(123)->twice();
+    }
+
+    public function testItThrowsIfItWasCalledMoreThanTheNumberOfTimesWeExpected(): void
+    {
+        $spy = spy(function () {
+        });
+
+        $spy();
+        $spy();
+        $spy();
+
+        $this->expectException(InvalidCountException::class);
+        $spy->shouldHaveBeenCalled()->twice();
+    }
+
+    public function testItThrowsIfItWasCalledMoreThanTheNumberOfTimesWeExpectedWithParticularArguments(): void
+    {
+        $spy = spy(function () {
+        });
+
+        $spy(123);
+        $spy(123);
+        $spy(123);
+
+        $this->expectException(InvalidCountException::class);
+        $spy->shouldHaveBeenCalled()->with(123)->twice();
+    }
+
+    public function testItThrowsIfItWasCalledWhenWeExpectedItToNotHaveBeenCalled(): void
+    {
+        $spy = spy(function () {
+        });
+
+        $spy();
+
+        $this->expectException(InvalidCountException::class);
+        $spy->shouldNotHaveBeenCalled();
+    }
+
+    public function testItThrowsIfItWasCalledWithTheArgsWeWereNotExpecting(): void
+    {
+        $spy = spy(function () {
+        });
+
+        $spy(123);
+
+        $this->expectException(InvalidCountException::class);
+        $spy->shouldNotHaveBeenCalled([123]);
+    }
+
+    public function testItThrowsIfTheArgumentsDoNotMatch(): void
+    {
+        $spy = spy(function () {
+        });
+
+        $spy(123);
+
+        $this->expectException(InvalidCountException::class);
+        $spy->shouldHaveBeenCalled()->with(123, 546);
+    }
+
+    public function testItThrowsIfTheCallableWasNotCalledAtAll(): void
+    {
+        $spy = spy(function () {
+        });
+
+        $this->expectException(InvalidCountException::class);
+        $spy->shouldHaveBeenCalled();
+    }
+
+    public function testItThrowsIfThereWereNoArgumentsButWeExpectedSome(): void
+    {
+        $spy = spy(function () {
+        });
+
+        $spy();
+
+        $this->expectException(InvalidCountException::class);
+        $spy->shouldHaveBeenCalled()->with(123, 546);
+    }
+
+    public function testItVerifiesItWasNotCalledWithSomeParticularArgumentsWhenCalledWithDifferentArgs(): void
+    {
+        $spy = spy(function () {
+        });
+
+        $spy(456);
+
+        $spy->shouldNotHaveBeenCalled([123]);
+    }
+
+    public function testItVerifiesItWasNotCalledWithSomeParticularArgumentsWhenCalledWithNoArgs(): void
+    {
+        $spy = spy(function () {
+        });
+
+        $spy();
+
+        $spy->shouldNotHaveBeenCalled([123]);
+    }
+
+    public function testItVerifiesTheClosureWasCalled(): void
+    {
+        $spy = spy(function () {
+        });
+
+        $spy();
+
+        $spy->shouldHaveBeenCalled();
+    }
+
+    public function testItVerifiesTheClosureWasNotCalled(): void
+    {
+        $spy = spy(function () {
+        });
+
+        $spy->shouldNotHaveBeenCalled();
+    }
+}
