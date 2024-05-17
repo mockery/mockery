d679019d640e1a3e12263026882cc11622b9e72c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:24 -0500

    Add InstanceMockPassTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/Generator/StringManipulation/Pass/InstanceMockPassTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\Mockery\Generator\StringManipulation\Pass;
+
+use Mockery\Generator\MockConfigurationBuilder;
+use Mockery\Generator\StringManipulation\Pass\InstanceMockPass;
+use PHPUnit\Framework\TestCase;
+use function mb_strpos;
+
+final class InstanceMockPassTest extends TestCase
+{
+    public function testShouldAppendConstructorAndPropertyForInstanceMock(): void
+    {
+        $builder = new MockConfigurationBuilder();
+        $builder->setInstanceMock(true);
+        $config = $builder->getMockConfiguration();
+        $pass = new InstanceMockPass();
+        $code = $pass->apply('class Dave { }', $config);
+
+        self::assertNotFalse(mb_strpos($code, 'public function __construct'));
+
+        self::assertNotFalse(mb_strpos($code, 'protected $_mockery_ignoreVerification'));
+
+        self::assertNotFalse(mb_strpos($code, 'this->_mockery_constructorCalled(func_get_args());'));
+    }
+}
