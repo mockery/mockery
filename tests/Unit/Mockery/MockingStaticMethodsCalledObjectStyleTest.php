2239b056be712b415aa1912e70c58a11393bf713
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:13 -0500

    Add MockingStaticMethodsCalledObjectStyleTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/MockingStaticMethodsCalledObjectStyleTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use PHP73\ClassWithStaticMethods;
+
+final class MockingStaticMethodsCalledObjectStyleTest extends MockeryTestCase
+{
+    public function testProtectedStaticMethodCalledObjectStyleMockWithNotAllowingMockingOfNonExistentMethods(): void
+    {
+        Mockery::getConfiguration()->allowMockingNonExistentMethods(false);
+        $mock = mock(ClassWithStaticMethods::class);
+        $mock->shouldAllowMockingProtectedMethods();
+        $mock->shouldReceive('protectedBar')->andReturn(true);
+        self::assertTrue($mock->protectedBar());
+        Mockery::getConfiguration()->allowMockingNonExistentMethods(true);
+    }
+
+    public function testStaticMethodCalledObjectStyleMock(): void
+    {
+        $mock = mock(ClassWithStaticMethods::class);
+        $mock->shouldReceive('foo')->andReturn(true);
+        self::assertTrue($mock->foo());
+    }
+
+    public function testStaticMethodCalledObjectStyleMockWithNotAllowingMockingOfNonExistentMethods(): void
+    {
+        Mockery::getConfiguration()->allowMockingNonExistentMethods(false);
+        $mock = mock(ClassWithStaticMethods::class);
+        $mock->shouldReceive('foo')->andReturn(true);
+        self::assertTrue($mock->foo());
+        Mockery::getConfiguration()->allowMockingNonExistentMethods(true);
+    }
+}
