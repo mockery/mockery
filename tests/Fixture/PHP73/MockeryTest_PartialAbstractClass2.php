ff87ce2db74e09fc5c9ab88baacfd9f02ce30e30
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:27:53 -0500

    Add MockeryTest_PartialAbstractClass2.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/PHP73/MockeryTest_PartialAbstractClass2.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PHP73;
+
+abstract class MockeryTest_PartialAbstractClass2
+{
+    abstract public function foo();
+
+    public function bar()
+    {
+        return 'abc';
+    }
+
+    abstract public function baz();
+}
