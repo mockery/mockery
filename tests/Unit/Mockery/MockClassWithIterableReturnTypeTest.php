7ce162cf97d08b9b0ae787987a1fbc89120ec777
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:19 -0500

    Add MockClassWithIterableReturnTypeTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/MockClassWithIterableReturnTypeTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use PHP73\ReturnTypeIterableTypeHint;
+
+final class MockClassWithIterableReturnTypeTest extends MockeryTestCase
+{
+    public function testMockingIterableReturnType(): void
+    {
+        $mock = mock(ReturnTypeIterableTypeHint::class);
+
+        $mock->expects('returnIterable');
+
+        self::assertSame([], $mock->returnIterable());
+    }
+}
