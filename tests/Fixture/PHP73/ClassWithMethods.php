5545b846789deeada2fe5b0fcc56cc793f7a5920
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:06 -0500

    Add ClassWithMethods.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/PHP73/ClassWithMethods.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHP73;
+
+class ClassWithMethods
+{
+    public function foo()
+    {
+        return 'foo';
+    }
+
+    public function bar()
+    {
+        return 'bar';
+    }
+}
