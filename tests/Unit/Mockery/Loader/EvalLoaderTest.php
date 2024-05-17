d2b3ce61c63c25a90924fac8ae52a0602d1068e5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-17 03:47:28 -0500

    Update EvalLoaderTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Mockery/Loader/EvalLoaderTest.php
+++ tests/Unit/Mockery/Loader/EvalLoaderTest.php
@@ -7,6 +7,9 @@ namespace Tests\Unit\Mockery\Loader;
 use Mockery\Loader\EvalLoader;
 use Mockery\Loader\Loader;
 
+/**
+ * @coversDefaultClass \Mockery
+ */
 final class EvalLoaderTest extends LoaderTestCase
 {
     public function getLoader(): Loader
