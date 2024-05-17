d2df541d2af5cff2bbc6a3aa256d13b2090808b3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:27:52 -0500

    Add MockeryTest_PartialNormalClass.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Fixture/PHP73/MockeryTest_PartialNormalClass.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PHP73;
+
+class MockeryTest_PartialNormalClass
+{
+    public function foo()
+    {
+        return 'abc';
+    }
+
+    public function bar()
+    {
+        return 'abc';
+    }
+}
