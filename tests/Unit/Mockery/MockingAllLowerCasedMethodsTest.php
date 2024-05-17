bb270ddeb416e0432c1f2a73fb8bb595713b74b8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:15 -0500

    Add MockingAllLowerCasedMethodsTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/MockingAllLowerCasedMethodsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use PHP73\ClassWithAllLowerCaseMethod;
+
+final class MockingAllLowerCasedMethodsTest extends MockeryTestCase
+{
+    public function testItShouldAllowToCallAllLowerCasedMethodAsCamelCased(): void
+    {
+        Mockery::getConfiguration()->allowMockingNonExistentMethods(true);
+
+        $expected = 'mocked';
+
+        $mock = mock(ClassWithAllLowerCaseMethod::class);
+
+        $mock->shouldReceive('userExpectsCamelCaseMethod')->andReturn($expected);
+
+        self::assertSame($expected, $mock->userExpectsCamelCaseMethod());
+    }
+}
