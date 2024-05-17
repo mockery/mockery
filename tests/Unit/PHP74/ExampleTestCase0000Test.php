06f5f8b35021529a32e8fb1297d3bdefc69ee126
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:33 -0500

    Add ExampleTestCase0000Test.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/PHP74/ExampleTestCase0000Test.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\PHP74;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+/**
+ * @coversDefaultClass Mockery
+ * @requires PHP 7.4
+ * @see https://github.com/mockery/mockery/issues/{id}
+ */
+final class ExampleTestCase0000Test extends MockeryTestCase
+{
+    public function testDescription(): void
+    {
+        $mock = Mockery::mock('ExampleClass');
+
+        $mock->expects('exampleMethod')->andReturns(true);
+
+        self::assertTrue($mock->exampleMethod());
+    }
+}
