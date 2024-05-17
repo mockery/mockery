da609388c38089a9acffc2e56bd12016ba05d7b2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-16 23:28:19 -0500

    Add MockClassWithMethodOverloadingTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Mockery/MockClassWithMethodOverloadingTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Mockery;
+
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Mockery\Exception\BadMethodCallException;
+use PHP73\TestWithMethodOverloading;
+use PHP73\TestWithMethodOverloadingWithoutCall;
+
+final class MockClassWithMethodOverloadingTest extends MockeryTestCase
+{
+    public function testCreateMockForClassWithMethodOverloading(): void
+    {
+        $mock = mock(TestWithMethodOverloading::class)->makePartial();
+
+        self::assertInstanceOf(TestWithMethodOverloading::class, $mock);
+
+        self::assertSame(42, $mock->theAnswer());
+    }
+
+    public function testCreateMockForClassWithMethodOverloadingWithExistingMethod(): void
+    {
+        $mock = mock(TestWithMethodOverloading::class)->makePartial();
+
+        self::assertInstanceOf(TestWithMethodOverloading::class, $mock);
+
+        self::assertSame(1, $mock->thisIsRealMethod());
+    }
+
+    public function testThrowsWhenMethodDoesNotExist(): void
+    {
+        $mock = mock(TestWithMethodOverloadingWithoutCall::class)->makePartial();
+
+        self::assertInstanceOf(TestWithMethodOverloadingWithoutCall::class, $mock);
+
+        $this->expectException(BadMethodCallException::class);
+
+        $mock->randomMethod();
+    }
+}
