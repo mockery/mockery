c01b4200792f993ae4933674ffa9054c9af0e782
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-02 15:54:19 -0500

    Update helpers.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- library/helpers.php
+++ library/helpers.php
@@ -15,55 +15,6 @@ use Mockery\Matcher\AndAnyOtherArgs;
 use Mockery\Matcher\AnyArgs;
 use Mockery\MockInterface;
 
-if (! \function_exists('mock')) {
-    /**
-     * @template TMock of object
-     *
-     * @param TMock|array<class-string<TMock>|TMock|Closure(LegacyMockInterface&MockInterface&TMock):LegacyMockInterface&MockInterface&TMock|array<TMock>> ...$args
-     *
-     * @return (LegacyMockInterface|MockInterface)&TMock
-     */
-    function mock(...$args)
-    {
-        return Mockery::mock(...$args);
-    }
-}
-
-if (! \function_exists('spy')) {
-    /**
-     * @template TSpy of object
-     *
-     * @param TSpy|array<class-string<TSpy>|TSpy|Closure(LegacyMockInterface&MockInterface&TSpy):LegacyMockInterface&MockInterface&TSpy|array<TSpy>> ...$args
-     *
-     * @return (LegacyMockInterface|MockInterface)&TSpy
-     */
-    function spy(...$args)
-    {
-        return Mockery::spy(...$args);
-    }
-}
-
-if (! \function_exists('namedMock')) {
-    /**
-     * @template TNamedMock of object
-     *
-     * @param TNamedMock|array<class-string<TNamedMock>|TNamedMock|array<TNamedMock>> ...$args
-     *
-     * @return (LegacyMockInterface|MockInterface)&TNamedMock
-     */
-    function namedMock(...$args)
-    {
-        return Mockery::namedMock(...$args);
-    }
-}
-
-if (! \function_exists('anyArgs')) {
-    function anyArgs(): AnyArgs
-    {
-        return new AnyArgs();
-    }
-}
-
 if (! \function_exists('andAnyOtherArgs')) {
     function andAnyOtherArgs(): AndAnyOtherArgs
     {
@@ -75,5 +26,171 @@ if (! \function_exists('andAnyOthers')) {
     function andAnyOthers(): AndAnyOtherArgs
     {
         return new AndAnyOtherArgs();
+    }
+}
+
+if (! \function_exists('anyArgs')) {
+    function anyArgs(): AnyArgs
+    {
+        return new AnyArgs();
+    }
+}
+
+if (! \function_exists('get_debug_type')) {
+    /**
+     * @param mixed $value
+     */
+    function get_debug_type($value): string
+    {
+        switch (true) {
+            case $value === null: return 'null';
+            case \is_bool($value): return 'bool';
+            case \is_string($value): return 'string';
+            case \is_array($value): return 'array';
+            case \is_int($value): return 'int';
+            case \is_float($value): return 'float';
+            case \is_object($value): break;
+            case $value instanceof \__PHP_Incomplete_Class: return '__PHP_Incomplete_Class';
+            default:
+                if (null === $type = @\get_resource_type($value)) {
+                    return 'unknown';
+                }
+
+                if ($type === 'Unknown') {
+                    $type = 'closed';
+                }
+
+                return "resource ({$type})";
+        }
+
+        $class = \get_class($value);
+
+        if (! \str_contains($class, '@')) {
+            return $class;
+        }
+
+        $parent = \get_parent_class($class);
+        if ($parent !== false) {
+            return $parent . '@anonymous';
+        }
+
+        $interfaces = \class_implements($class);
+        if ($interfaces === false) {
+            return 'class@anonymous';
+        }
+
+        $parent = \key($interfaces);
+        if ($parent === null) {
+            return 'class@anonymous';
+        }
+
+        return $parent . '@anonymous';
+    }
+}
+
+if (! \function_exists('mock')) {
+    /**
+     * @template TMixed
+     *
+     * @param TMixed ...$args
+     *
+     * @throws Throwable
+     *
+     * @return LegacyMockInterface|MockInterface
+     */
+    function mock(...$args)
+    {
+        return Mockery::mock(...$args);
+    }
+}
+
+if (! \function_exists('namedMock')) {
+    /**
+     * @template TMixed
+     *
+     * @param TMixed ...$args
+     *
+     * @return ((LegacyMockInterface&TMixed)|(TMixed&MockInterface))
+     *
+     * @throws Throwable
+     *
+     */
+    function namedMock(...$args)
+    {
+        return Mockery::namedMock(...$args);
+    }
+}
+
+if (! \function_exists('spy')) {
+    /**
+     * @template TMixed
+     *
+     * @param TMixed ...$args
+     *
+     * @return ((LegacyMockInterface&TMixed)|(TMixed&MockInterface))
+     *
+     * @throws Throwable
+     */
+    function spy(...$args)
+    {
+        return Mockery::spy(...$args);
+    }
+}
+
+if (! \function_exists('str_contains')) {
+    /**
+     * Copied from symfony/polyfill (https://github.com/symfony/polyfill/blob/1.x/src/Php80/Php80.php)
+     *
+     * @copyright Fabien Potencier
+     * @license https://github.com/symfony/polyfill/blob/1.x/src/Php80/LICENSE
+     *
+     * @param non-empty-string $haystack
+     * @param non-empty-string $needle
+     */
+    function str_contains(string $haystack, string $needle): bool
+    {
+        return '' === $needle || false !== strpos($haystack, $needle);
+    }
+}
+
+if (! \function_exists('str_ends_with')) {
+    /**
+     * Copied from symfony/polyfill (https://github.com/symfony/polyfill/blob/1.x/src/Php80/Php80.php)
+     *
+     * @copyright Fabien Potencier
+     * @license https://github.com/symfony/polyfill/blob/1.x/src/Php80/LICENSE
+     *
+     * @param non-empty-string $haystack
+     * @param non-empty-string $needle
+     */
+    function str_ends_with(string $haystack, string $needle): bool
+    {
+        if ('' === $needle || $needle === $haystack) {
+            return true;
+        }
+
+        if ('' === $haystack) {
+            return false;
+        }
+
+        $needleLength = \strlen($needle);
+
+        return $needleLength <= \strlen($haystack) && 0 === substr_compare($haystack, $needle, -$needleLength);
+    }
+}
+
+if (! \function_exists('str_starts_with')) {
+    /**
+     * Copied from symfony/polyfill (https://github.com/symfony/polyfill/blob/1.x/src/Php80/Php80.php)
+     *
+     * @copyright Fabien Potencier
+     * @license https://github.com/symfony/polyfill/blob/1.x/src/Php80/LICENSE
+     *
+     * @param non-empty-string $haystack
+     * @param non-empty-string $needle
+     */
+    function str_starts_with(string $haystack, string $needle): bool
+    {
+        return 0 === strncmp($haystack, $needle, \strlen($needle));
     }
 }
