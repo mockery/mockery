338b659ae027f236f070609e51aa67e2275e1ca3
Jenthe Noordsij <j.noordsij@sslleiden.nl>
2026-08-17 11:43:42 +0200

    Use symfony/polyfill-php80 instead of manual copies

--- library/helpers.php
+++ library/helpers.php
@@ -35,63 +35,6 @@ if (! \function_exists('anyArgs')) {
     }
 }
 
-if (! \function_exists('get_debug_type')) {
-    /**
-     * Copied from symfony/polyfill (https://github.com/symfony/polyfill/blob/1.x/src/Php80/Php80.php)
-     *
-     * @copyright Fabien Potencier
-     * @license https://github.com/symfony/polyfill/blob/1.x/src/Php80/LICENSE
-     *
-     * @param mixed $value
-     */
-    function get_debug_type($value): string
-    {
-        switch (true) {
-            case null === $value: return 'null';
-            case \is_bool($value): return 'bool';
-            case \is_string($value): return 'string';
-            case \is_array($value): return 'array';
-            case \is_int($value): return 'int';
-            case \is_float($value): return 'float';
-            case \is_object($value): break;
-            case $value instanceof \__PHP_Incomplete_Class: return '__PHP_Incomplete_Class';
-            default:
-                if (null === $type = @\get_resource_type($value)) {
-                    return 'unknown';
-                }
-
-                if ('Unknown' === $type) {
-                    $type = 'closed';
-                }
-
-                return "resource ({$type})";
-        }
-
-        $class = \get_class($value);
-
-        if (! \str_contains($class, '@')) {
-            return $class;
-        }
-
-        $parent = \get_parent_class($class);
-        if (false !== $parent) {
-            return $parent . '@anonymous';
-        }
-
-        $interfaces = \class_implements($class);
-        if (false === $interfaces) {
-            return 'class@anonymous';
-        }
-
-        $parent = \key($interfaces);
-        if (null === $parent) {
-            return 'class@anonymous';
-        }
-
-        return $parent . '@anonymous';
-    }
-}
-
 if (! \function_exists('mock')) {
     /**
      * @param  mixed         ...$args
@@ -128,67 +71,6 @@ if (! \function_exists('spy')) {
     function spy(...$args)
     {
         return Mockery::spy(...$args);
-    }
-}
-
-if (! \function_exists('str_contains')) {
-    /**
-     * Copied from symfony/polyfill (https://github.com/symfony/polyfill/blob/1.x/src/Php80/Php80.php)
-     *
-     * @copyright Fabien Potencier
-     * @license https://github.com/symfony/polyfill/blob/1.x/src/Php80/LICENSE
-     *
-     * @param non-empty-string $haystack
-     * @param non-empty-string $needle
-     */
-    function str_contains(string $haystack, string $needle): bool
-    {
-        return '' === $needle || \strpos($haystack, $needle) !== false;
-    }
-}
-
-if (! \function_exists('str_ends_with')) {
-    /**
-     * Modified copy from symfony/polyfill (https://github.com/symfony/polyfill/blob/1.x/src/Php80/Php80.php)
-     *
-     * @copyright Fabien Potencier
-     * @license https://github.com/symfony/polyfill/blob/1.x/src/Php80/LICENSE
-     *
-     * @param non-empty-string $haystack
-     * @param non-empty-string $needle
-     */
-    function str_ends_with(string $haystack, string $needle): bool
-    {
-        if ('' === $needle || $needle === $haystack) {
-            return true;
-        }
-
-        if ('' === $haystack) {
-            return false;
-        }
-
-        $needleLength = \strlen($needle);
-        if ($needleLength > \strlen($haystack)) {
-            return false;
-        }
-
-        return \substr_compare($haystack, $needle, -$needleLength) === 0;
-    }
-}
-
-if (! \function_exists('str_starts_with')) {
-    /**
-     * Copied from symfony/polyfill (https://github.com/symfony/polyfill/blob/1.x/src/Php80/Php80.php)
-     *
-     * @copyright Fabien Potencier
-     * @license https://github.com/symfony/polyfill/blob/1.x/src/Php80/LICENSE
-     *
-     * @param non-empty-string $haystack
-     * @param non-empty-string $needle
-     */
-    function str_starts_with(string $haystack, string $needle): bool
-    {
-        return \strncmp($haystack, $needle, \strlen($needle)) === 0;
     }
 }
 
